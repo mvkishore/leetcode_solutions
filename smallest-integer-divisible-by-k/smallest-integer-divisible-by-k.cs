919653d1997b913abee13d77dde33e3981754f34
Venkata Kishore Marisetty <kishore9700@gmail.com>
2021-12-30 22:09:25 +0530

    Time: 28 ms (100.00%), Space: 26.9 MB (66.67%) - LeetHub

---
+++ smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int SmallestRepunitDivByK(int K) {
+         int remainder = 0;
+        for (int length_N = 1; length_N <= K; length_N++) {
+            remainder = (remainder * 10 + 1) % K;
+            if (remainder == 0) {
+                return length_N;
+            }
+        }
+        return -1;
+    }
+}
