3224b97568a5d9c5d0ca64607e55096fd91a5fd6
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-10 20:10:50 +0530

    Time: 112 ms (22.95%), Space: 35.8 MB (93.83%) - LeetHub

--- 67-add-binary/67-add-binary.cs
+++ 67-add-binary/67-add-binary.cs
@@ -2,19 +2,16 @@ public class Solution {
     public string AddBinary(string a, string b) {
         StringBuilder sb = new StringBuilder();
         int p1 = a.Length - 1, p2 = b.Length - 1;
-        int carry = 0;
+        int sum = 0;
         
-        while(p1 >= 0 || p2 >= 0){
-            int sum = (p1 >= 0 ? (a[p1--] - '0') : 0)
-                        + (p2 >=0 ? (b[p2--] - '0') : 0) + carry;
+        while(p1 >= 0 || p2 >= 0 || sum > 0){
+            sum += p1 >= 0 ? (a[p1--] - '0') : 0;
+            sum += p2 >=0 ? (b[p2--] - '0') : 0;
             
             sb.Insert(0, (sum % 2).ToString());
-            carry = sum / 2;
+            sum = sum / 2;
         }
-               
-        if(carry > 0)
-            sb.Insert(0, carry.ToString());
-            
+        
         return sb.ToString();
     }
 }
