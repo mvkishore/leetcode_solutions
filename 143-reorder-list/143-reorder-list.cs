b03cd85deed1371b584e840dd86c025f089c9add
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-12 15:55:15 +0530

    Time: 125 ms (21.40%), Space: 39.4 MB (95.59%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.cs
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public void ReorderList(ListNode head) {
+        ListNode slow = head, fast = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+       
+        ListNode prev = null, cur = slow; 
+        while(cur != null){
+            var next = cur.next; 
+            cur.next = prev; 
+            
+            prev = cur; 
+            cur = next; 
+        }
+        
+        ListNode left = head, right = prev;
+        while(right.next != null){
+            var next = left.next; 
+            left.next = right; 
+            left = next; 
+            
+            next = right.next;
+            right.next = left;
+            right = next; 
+        }
+    }
+}
