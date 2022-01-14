a409b15956e3f0d714e8381dbf0750fd2e7db436
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-14 23:05:37 +0530

    Time: 480 ms (16.47%), Space: 48.8 MB (81.18%) - LeetHub

--- 1891-cutting-ribbons/1891-cutting-ribbons.cs
+++ 1891-cutting-ribbons/1891-cutting-ribbons.cs
@@ -2,18 +2,18 @@ public class Solution {
     public int MaxLength(int[] ribbons, int k) {
         int n = ribbons.Length;
         
-        int low = 1, hi = (int) 1e5 + 1;
+        int low = 0, hi = (int) 1e5;
      
         while(low < hi){
-            int mid = low + (hi - low) / 2;
+            int mid = hi - (hi - low) / 2;
             
             int r = TotalRibbons(mid, ribbons);
             if(r >= k)
-                low = mid + 1;
+                low = mid;
             else
-                hi = mid;
+                hi = mid - 1;
         }
-        return low - 1;
+        return low;
     }
     
     public int TotalRibbons(int size, int[] ribbons)
