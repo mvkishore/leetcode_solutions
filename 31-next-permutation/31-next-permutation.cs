5c60f2adff6aedd5776fb3e90a0a13f32a1a46dc
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-15 22:44:07 +0530

    Time: 116 ms (93.47%), Space: 41.8 MB (19.48%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public void NextPermutation(int[] nums) {
+        int n = nums.Length;
+        
+        int explorePos = n - 1;
+        while(explorePos > 0 && nums[explorePos - 1] >= nums[explorePos])
+            explorePos--;
+        
+        explorePos--;
+        
+        if(explorePos >= 0){
+            int nextPos = explorePos + 1;
+            while(nextPos < n && nums[nextPos] > nums[explorePos])
+                nextPos++;
+            
+            Swap(nums, explorePos, nextPos - 1);
+        }
+        Reverse(nums, explorePos + 1, n-1);
+    }
+    
+    private void Reverse(int[] nums, int start, int end)
+    {
+        while(start < end){
+            Swap(nums, start++, end--);
+        }
+    }
+    
+    private void Swap(int[] nums, int i, int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
