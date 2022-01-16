117440e36cc7ff2b68044c15b9886186144d5723
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-16 08:02:37 +0530

    Time: 113 ms (31.30%), Space: 34.7 MB (94.35%) - LeetHub

---
+++ 791-custom-sort-string/791-custom-sort-string.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public string CustomSortString(string order, string s) {
+        int[] counter = new int[26];
+        
+        foreach(var c in s)
+            counter[c - 'a']++;
+        StringBuilder sb = new StringBuilder();
+        
+        foreach(var c in order){
+            while(counter[c - 'a'] > 0){
+                sb.Append(c);
+                counter[c - 'a']--;
+            }
+        }
+        
+        for(int i=0; i<26; i++){
+            while(counter[i] > 0){
+                sb.Append((char)(i + 'a'));
+                counter[i]--;
+            }
+        }
+        return sb.ToString();
+    }
+}
