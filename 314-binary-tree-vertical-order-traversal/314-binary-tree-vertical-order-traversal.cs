812efc52fba82ec93e2ec1baae63b3f32ebccd36
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-16 12:36:12 +0530

    Time: 164 ms (37.98%), Space: 39.7 MB (94.32%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.cs
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.cs
@@ -21,31 +21,44 @@
  
  */
 public class Solution {
-    int minCol = int.MaxValue, maxCol = int.MinValue;
     public IList<IList<int>> VerticalOrder(TreeNode root) {
-        Dictionary<int, List<(int row, int val)>> verticals = new Dictionary<int, List<(int row, int val)>>();
-        
-        VerticalOrder(root, verticals, 0, 0);
-        IList<IList<int>> result = new List<IList<int>>();
-        for(int i=minCol; i<=maxCol; i++){
-            result.Add(verticals[i].OrderBy(x=>x.row).Select(x=>x.val).ToList());
-        }
-        return result;
-    }
-    
-    private void VerticalOrder(TreeNode root, Dictionary<int, List<(int row, int val)>> verticals, int col, int row)
-    {
         if(root == null)
-            return;
+            return new List<IList<int>>();
         
-        if(!verticals.ContainsKey(col))
-            verticals.Add(col, new List<(int row, int val)>());
+        Dictionary<int, List<int>> cols = new Dictionary<int, List<int>>();
+        Queue<(TreeNode node, int col)> queue = new Queue<(TreeNode node, int col)>();
+        queue.Enqueue((root, 0));
         
-        minCol = Math.Min(minCol, col);
-        maxCol = Math.Max(maxCol, col);
+        int minCol = 0, maxCol = 0;
+        while(queue.Count > 0){
+            int size = queue.Count;
+            
+            for(int i= 0; i < size ; i++){
+                var cur = queue.Dequeue();
+                
+                int col = cur.col;
+                int val = cur.node.val;
+                
+                if(!cols.ContainsKey(col))
+                    cols.Add(col, new List<int>());
+                
+                cols[col].Add(val);
+                
+                if(cur.node.left != null)
+                    queue.Enqueue((cur.node.left, col - 1));
+                
+                if(cur.node.right != null)
+                    queue.Enqueue((cur.node.right, col + 1));
+                                  
+                minCol = Math.Min(minCol, col);
+                maxCol = Math.Max(maxCol, col);
+            }
+        }
         
-        verticals[col].Add((row, root.val));
-        VerticalOrder(root.left, verticals, col - 1, row + 1);
-        VerticalOrder(root.right, verticals, col + 1, row + 1);
+        IList<IList<int>> res = new List<IList<int>>();
+        for(int i=minCol; i<= maxCol; i++)
+            res.Add(cols[i]);
+        
+        return res;
     }
 }
