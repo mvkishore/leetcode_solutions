c1061b7f1373b0ac666a0f951fe29def87b6d156
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-16 10:52:06 +0530

    Time: 804 ms (72.51%), Space: 141.4 MB (6.73%) - LeetHub

--- 146-lru-cache/146-lru-cache.cs
+++ 146-lru-cache/146-lru-cache.cs
@@ -1,47 +1,92 @@
 public class LRUCache {
-    Dictionary<int, LinkedListNode<(int key, int val)>> cache;
-    LinkedList<(int key, int val)> list;
+    Dictionary<int, Node> cache;
+    Node head, tail;
     int size;
     int capacity;
     public LRUCache(int capacity) {
-        cache = new Dictionary<int, LinkedListNode<(int key, int val)>>(capacity);
+        cache = new Dictionary<int, Node>(capacity);
         this.capacity = capacity;
-        list = new LinkedList<(int key, int val)>();
+        
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        
+        head.next = tail;
+        tail.prev = head;
+        
         this.size = 0;
+    }
+    
+    private void AddFirst(Node node){
+        var next = head.next;
+        
+        head.next = node;
+        node.next = next;
+        
+        next.prev = node;
+        node.prev = head;
+    }
+    
+    private void Remove(Node node)
+    {
+        var next = node.next;
+        var prev = node.prev;
+        
+        next.prev = prev;
+        prev.next = next;
+    }
+    
+    private Node RemoveLast()
+    {
+        var last = tail.prev;
+        Remove(last);
+        return last;
+    }
+    
+    private void MoveToFirst(Node node)
+    {
+        Remove(node);
+        AddFirst(node);
     }
     
     public int Get(int key) {
         if(!cache.ContainsKey(key))
             return -1;
-        
         var node = cache[key];
-        list.Remove(node);
-        
-        list.AddFirst(new LinkedListNode<(int key, int val)>((key, node.Value.val)));
-        cache[key] = list.First;
-        return node.Value.val;
+        MoveToFirst(node);
+        return node.value;
     }
     
     public void Put(int key, int value) {
-        var newNode = new LinkedListNode<(int key, int val)>((key, value));
-        
-        if(cache.ContainsKey(key)) {
+        if(cache.ContainsKey(key))
+        {
             var node = cache[key];
-            list.Remove(node);
-        } 
-        else if(size == capacity){
-            var last = list.Last;
-            list.Remove(list.Last);
-            cache.Remove(last.Value.key);
+            node.value = value;
+            MoveToFirst(node);
+        } else {
+            var node = new Node(key, value);
+            if(size == capacity){
+                var last = RemoveLast();
+                cache.Remove(last.key);
+            }
+            else size++;
+            AddFirst(node);
+            cache.Add(key, node);
         }
-        else size++;
-
-        list.AddFirst(newNode);
-        cache[key] = list.First;
     }
     
-    
-    
+    public class Node
+    {
+        public int key;
+        public int value;
+        public Node prev;
+        public Node next;
+        
+        public Node(int key, int value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+    }
 }
 
 /**
