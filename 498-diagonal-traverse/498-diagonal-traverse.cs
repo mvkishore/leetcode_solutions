7e42dc0b41f42856b59bb1ecdcb34d41bd56daf7
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-16 16:14:14 +0530

    Time: 140 ms (97.97%), Space: 48 MB (10.98%) - LeetHub

--- 498-diagonal-traverse/498-diagonal-traverse.cs
+++ 498-diagonal-traverse/498-diagonal-traverse.cs
@@ -1,27 +1,28 @@
 public class Solution {
     public int[] FindDiagonalOrder(int[][] mat) {
         List<int> result = new List<int>();
+        
         Traverse(mat, 0, 0, 1, result);
         return result.ToArray();
     }
     
-    private void Traverse(int[][] mat, int row, int col, int dir, List<int> result)
+    private void Traverse(int[][] mat, int row, int col, int dir, IList<int> result)
     {
         int rows = mat.Length;
         int cols = mat[0].Length;
         
-        result.Add(mat[row][col]);
-        
         if(result.Count == rows * cols)
             return;
         
-        if(dir == 1 && col == cols -1)
+        result.Add(mat[row][col]);
+        
+        if(dir == 1 && row < rows && col == cols - 1)
             Traverse(mat, row + 1, col, -dir, result);
-        else if(dir == 1 && row == 0)
+        else if(dir == 1 && col < cols && row == 0)
             Traverse(mat, row, col + 1, -dir, result);
-        else if(dir == -1 && row == rows - 1)
+        else if(dir == -1 && col < cols && row == rows -1)
             Traverse(mat, row, col + 1, -dir, result);
-        else if(dir == -1 && col == 0)
+        else if(dir == -1 && row < rows && col == 0)
             Traverse(mat, row + 1, col, -dir, result);
         else
             Traverse(mat, row - dir, col + dir, dir, result);
