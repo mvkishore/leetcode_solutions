d49140516feeb7d90549f75c0748d6b8f1996b9b
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-17 15:13:10 +0530

    Time: 375 ms (26.17%), Space: 45.4 MB (87.05%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cs
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cs
@@ -1,11 +1,11 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
-        int buy = int.MinValue;
+        int buy = int.MaxValue;
         int sell = 0;
         
         for(int i=0; i<prices.Length; i++){
-            buy = Math.Max(buy, -prices[i]);
-            sell = Math.Max(sell, buy + prices[i]);
+            buy = Math.Min(buy, prices[i]);
+            sell = Math.Max(sell, prices[i] - buy);
         }
         
         return sell;
