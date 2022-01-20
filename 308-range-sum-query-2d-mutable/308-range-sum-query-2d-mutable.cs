aca2df24ee55d4651e3da35f83bf1192d6dc768e
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-20 08:27:27 +0530

    Time: 282 ms (10.00%), Space: 49.6 MB (90.00%) - LeetHub

--- 308-range-sum-query-2d-mutable/308-range-sum-query-2d-mutable.cs
+++ 308-range-sum-query-2d-mutable/308-range-sum-query-2d-mutable.cs
@@ -5,35 +5,31 @@ public class NumMatrix {
         int m = matrix.Length;
         int n = matrix[0].Length;
         this.matrix = matrix;
-        PopulateCache();
-    }
-    public void Update(int uRow, int uCol, int val) {
-        matrix[uRow][uCol] = val;
-        PopulateCache();
-    }
-    
-    private void PopulateCache()
-    {
-        int m = matrix.Length;
-        int n = matrix[0].Length;
+        
         cache = new int[m, n];
         for(int row=0; row < m; row++){
+            int sum =0;
             for(int col=0; col< n; col++){
-                if(row == 0){
-                    cache[row, col] = ((col > 0) ? cache[row, col - 1] : 0) + matrix[row][col];
-                    continue;
-                }
-                if(col == 0){
-                    cache[row, col] = ((row > 0) ? cache[row - 1, col] : 0) + matrix[row][col];
-                    continue;
-                }
-                
-                if(row > 0 && col > 0)
-                    cache[row, col] = cache[row - 1, col] + cache[row , col - 1] + matrix[row][col] - cache[row-1, col-1];
+                sum += matrix[row][col];
+                cache[row, col] = sum + ((row > 0) ? cache[row-1, col] : 0);
             }
         }
     }
-   
+    public void Update(int uRow, int uCol, int val) {
+        var prev = matrix[uRow][uCol];
+        var diff = prev - val;
+        
+        matrix[uRow][uCol] = val;
+        
+        int m = matrix.Length;
+        int n = matrix[0].Length;
+        for(int row=uRow; row < m; row++){
+            for(int col=uCol; col< n; col++){
+                cache[row, col] = cache[row, col] - diff;
+            }
+        }
+    }
+    
     public int SumRegion(int row1, int col1, int row2, int col2) {
         int OA = (row1 > 0 && col1 > 0 ) ? cache[row1 - 1, col1 - 1] : 0;
         int OB = (row1 > 0) ? cache[row1 - 1, col2] : 0;
