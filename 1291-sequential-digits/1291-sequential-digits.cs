f9efca5abe4d13ac6c6434d90d85cbcc1b38205f
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-24 07:14:48 +0530

    Time: 148 ms (5.88%), Space: 32.8 MB (58.82%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.cs
@@ -0,0 +1,69 @@
+/*
+
+11  15
+
+12
+23
+34
+45
+56
+78
+89
+
+123
+234
+345
+456
+567
+678
+789
+
+
+
+132 --> 100 -
+
+132-- 999,      =
+1000 --- 9999,  =
+10000 -- 13000  =
+
+
+Sequential digits: 
+
+
+
+*/
+
+public class Solution {
+    public IList<int> SequentialDigits(int low, int high) {
+        int lowLen = low.ToString().Length, highLen = high.ToString().Length;
+        IList<int> res = new List<int>();
+        if(lowLen == highLen){
+            GetSequentialDigits(low, lowLen, high, res);
+        }else{
+            GetSequentialDigits(low, lowLen, int.MaxValue, res);
+            for(int i=lowLen+1; i< highLen; i++)
+                GetSequentialDigits(int.MinValue, i, int.MaxValue, res);
+            GetSequentialDigits(int.MinValue, highLen, high, res);
+        }
+        return res;
+    }
+    
+    private void GetSequentialDigits(int low, int len, int high, IList<int> res)
+    {
+        var digits = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int i=0;
+        while(i + len <= 9){
+            int l = 0;
+            int num = 0;
+            while(l < len){
+                num = num * 10 + digits[i + l];
+                l++;
+            }
+            if(num >= low && num <= high)
+                res.Add(num);
+            i++;//1
+        }
+    }
+    
+    
+}
