76c65ea776afd953b39440515a005afda9cbc1b9
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-25 12:23:00 +0530

    Time: 125 ms (68.42%), Space: 35.3 MB (96.32%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cs
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cs
@@ -1,48 +1,28 @@
-  
-/*
-
-  
-babad
-  0   1   2   3   4
-0 T   
-1     T
-2         T
-3             T
-4                  T
-
-
-*/
-
 public class Solution {
     public string LongestPalindrome(string s) {
-        int n = s.Length;
-        int maxLen = 0;
-        int startPos = -1;
+        int n = s.Length, maxLen = -1, startIndex = -1;
         
-        for(int i=0; i<n; i++)
-        {
-            var lenPos1 = ExpandForPalindrome(s, i, i);
-            var lenPos2 = ExpandForPalindrome(s, i, i+1);
+        for(int i=0; i<n; i++){
+            int oddLen = GetPalindromeLen(s, i, i);
+            int eveLen = GetPalindromeLen(s, i, i + 1);
             
-            var lenPos = lenPos1[0] < lenPos2[0] ? lenPos2 : lenPos1;
-            
-            if(lenPos[0] > maxLen){
-                maxLen = lenPos[0];
-                startPos = lenPos[1];
+            int len = Math.Max(oddLen, eveLen);
+            if(maxLen < len){
+                maxLen = len;
+                startIndex = i - (len - 1) / 2;
             }
         }
-        
-        return s.Substring(startPos, maxLen);
+        Console.WriteLine(startIndex);
+        return s.Substring(startIndex, maxLen);
     }
-    private int[] ExpandForPalindrome(string s, int l, int r)
+    
+    private int GetPalindromeLen(string s, int l, int r)
     {
         int n = s.Length;
-        while(l>=0 && r < n && s[l] == s[r])
-        {
+        while(l >= 0 && r < n && s[l] == s[r]){
             l--;
             r++;
         }
-        return new int[] {r - l - 1, l + 1};
+        return r - l - 1;
     }
-        
 }
