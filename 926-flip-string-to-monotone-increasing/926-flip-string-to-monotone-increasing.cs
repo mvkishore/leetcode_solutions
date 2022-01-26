570c09ff7a019b8ed411950cf0a2ceb496d1a207
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-26 13:24:10 +0530

    Time: 108 ms (41.92%), Space: 40.8 MB (61.08%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cs
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cs
@@ -1,20 +1,13 @@
-/*
-   max 0 index, => 1
-    min 1 index => 15 
-
-*/
-
 public class Solution {
     public int MinFlipsMonoIncr(string s) {
         int n = s.Length;
+        int[] f0 = new int[n + 1], f1 = new int[n+1];
         
-        int[] f0 = new int[n + 1], f1 = new int[n + 1];
-        f0[0] = f1[n] = 0;
+        for(int i=1; i<=n; i++)
+            f0[i] = f0[i-1] + (s[i-1] == '1' ? 1 : 0);
         
-        for(int i=1, j = n-1; i <= n; i++,j--){
-            f0[i] = f0[i-1] + ((s[i - 1] == '1' ? 1 : 0));
-            f1[j] = f1[j+1] + ((s[j] == '0' ? 1 : 0));
-        }
+        for(int j=n-1; j >= 0; j--)
+            f1[j] = f1[j+1] + (s[j] == '0' ? 1 : 0);
         
         int flips = n;
         for(int i=0; i<=n; i++){
