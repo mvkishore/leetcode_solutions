b7a3d9950a7eb7983af02510456b7ade74f9989b
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-27 07:35:03 +0530

    Time: 352 ms (61.29%), Space: 44.4 MB (25.81%) - LeetHub

--- 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cs
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cs
@@ -1,14 +1,12 @@
 public class Solution {
     public int ConnectSticks(int[] sticks) {
         SortedSet<(int val, int idx)> heap = new SortedSet<(int val, int idx)>();
-        int i=0, n = sticks.Length;
-        
-        if(n <= 1)
-            return 0;
+        int i=0;
         
         foreach(var stick in sticks)
             heap.Add((stick, i++));
-        int res = 0;
+        
+        int cost = 0;
         while(heap.Count > 1){
             var min1 = heap.Min;
             heap.Remove(min1);
@@ -16,9 +14,9 @@ public class Solution {
             heap.Remove(min2);
             
             (int val, int idx) newStick = (min1.val + min2.val, min1.idx);
-            res += newStick.val;
+            cost += newStick.val;
             heap.Add(newStick);
         }
-        return res;
+        return cost;
     }
 }
