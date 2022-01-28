e94f597c9eda55701f8bc8c453cbec32c46771a3
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-28 08:14:39 +0530

    Time: 113 ms (62.86%), Space: 37.6 MB (59.97%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cs
+++ 10-regular-expression-matching/10-regular-expression-matching.cs
@@ -14,30 +14,33 @@ prev=./ alphabet
 
 public class Solution {
     public bool IsMatch(string s, string p) {
-        
-        return IsMatch(0, s, 0, p);
+        bool?[,] memo = new bool?[s.Length + 1, p.Length + 1];
+        return IsMatch(0, s, 0, p, memo).Value;
     }
     
-    private bool IsMatch(int si, string s, int pi, string p)
+    private bool? IsMatch(int si, string s, int pi, string p,bool?[,] memo)
     {
         int sl = s.Length, pl = p.Length;
         
         if(pi == pl) 
             return si == sl;
         
+        if(memo[si, pi] != null)
+            return memo[si, pi];
+        
         if(pi+1 < pl && p[pi + 1] == '*'){
             //0 matches for current
-            if(IsMatch(si, s, pi + 2, p))
-                return true;
+            if(IsMatch(si, s, pi + 2, p, memo) == true)
+                return memo[si, pi] = true;
             
             //x matches for current
             if(CheckSingle(si, s, pi, p))
-                return IsMatch(si + 1, s, pi, p);
+                return memo[si, pi] = IsMatch(si + 1, s, pi, p, memo);
         }
         else if(CheckSingle(si, s, pi, p))
-           return IsMatch(si + 1, s, pi + 1, p);
+           return memo[si, pi] = IsMatch(si + 1, s, pi + 1, p, memo);
            
-        return false;
+        return memo[si, pi] = false;
     }
     
     private bool CheckSingle(int si, string s, int pi, string p)
