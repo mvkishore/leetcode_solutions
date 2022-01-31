ecfcfb06f4bf4a506db4aafb35620d9490b86ba6
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-01-31 09:29:17 +0530

    Time: 196 ms (100.00%), Space: 46.9 MB (8.22%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cs
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cs
@@ -1,21 +1,21 @@
 public class Solution {
     public int NumberOfSubarrays(int[] nums, int k) {
-        return NumberOfSubarraysAtMost(nums, k) - NumberOfSubarraysAtMost(nums, k-1);
-    }
-    
-    private int NumberOfSubarraysAtMost(int[] nums, int k){
-        int l = 0, r = 0, count =0, n = nums.Length;
+        int l=0, r=0, count =0, n = nums.Length, res = 0;
         
         while(r < n){
-            if(nums[r++] % 2 == 1)
+            if(nums[r++] % 2 == 1){
                 k--;
-            while(k < 0){
-                if(nums[l++] % 2 == 1)
-                    k++;
+                count = 0;
             }
             
-            count += r - l;
+            while(k == 0){
+                count++;
+                if(nums[l++] % 2 == 1){
+                    k++;
+                }
+            }
+            res += count;
         }
-        return count;
+        return res;
     }
 }
