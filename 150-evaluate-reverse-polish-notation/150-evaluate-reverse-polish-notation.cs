aab4c5a5cf982b68e93068c7de03fa5ea2375d48
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-02 11:20:13 +0530

    Time: 148 ms (29.36%), Space: 38.6 MB (55.85%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cs
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cs
@@ -1,32 +1,19 @@
 public class Solution {
     public int EvalRPN(string[] tokens) {
         Stack<int> stack = new Stack<int>();
+        Dictionary<string, Func<int, int, int>> operations = new Dictionary<string, Func<int, int, int>>();
+        operations.Add("+", (a, b) => a + b);
+        operations.Add("-", (a, b) => a - b);
+        operations.Add("*", (a, b) => a * b);
+        operations.Add("/", (a, b) => a / b);
+        
         for(int i=0; i < tokens.Length; i++){
-            switch(tokens[i]){
-                case "+" :
-                    int a = stack.Pop();
-                    int b = stack.Pop();
-                    stack.Push(a + b);
-                    break;
-                case "-":
-                    a = stack.Pop();
-                    b = stack.Pop();
-                    stack.Push(b - a);
-                    break;
-                case "*":
-                    a = stack.Pop();
-                    b = stack.Pop();
-                    stack.Push(a * b);
-                    break;
-                case "/" :
-                    a = stack.Pop();
-                    b = stack.Pop();
-                    stack.Push(b / a);
-                    break;
-                default:
-                    stack.Push(Convert.ToInt32(tokens[i]));
-                    break;
+            if(operations.ContainsKey(tokens[i])){
+                int b = stack.Pop();
+                int a = stack.Pop();
+                stack.Push(operations[tokens[i]](a, b));
             }
+            else stack.Push(Convert.ToInt32(tokens[i]));
         }
         
         return stack.Pop();
