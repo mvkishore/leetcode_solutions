aff57efb099bdd4312bfdf19d1e178126c16844e
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-05 08:28:50 +0530

    Time: 133 ms (68.54%), Space: 40.8 MB (50.73%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cs
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeKLists(ListNode[] lists) {
+        SortedSet<(int val, int idx, ListNode node)> heap = new SortedSet<(int val, int idx, ListNode node)>();
+        ListNode dummyHead = new ListNode();
+        ListNode trav = dummyHead;
+        int k=0;
+        for(int i=0; i<lists.Length; i++){
+            if(lists[i] != null)
+                heap.Add((lists[i].val, k++, lists[i]));
+        }
+        
+        while(heap.Count > 0){
+            var min = heap.Min;
+            var node = min.node;
+            var next = node.next;
+            trav.next = node;
+            
+            if(next != null)
+                heap.Add((next.val, k++, next));
+            heap.Remove(min);
+            
+            trav = trav.next;
+        }
+        
+        return dummyHead.next;
+    }
+}
