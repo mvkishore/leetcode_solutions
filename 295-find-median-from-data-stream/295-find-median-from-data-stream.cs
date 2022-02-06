65c0aebf4479b489afba8ee61449a84a51adce0d
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-06 11:02:22 +0530

    Time: 1163 ms (30.09%), Space: 97.3 MB (13.27%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cs
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cs
@@ -1,9 +1,3 @@
-/* 
-3 4 5 2 1 6 9 2 4 1 10
-
-1 1 2 2 3 4 |  4 5 6 9 10
-*/
-
 public class MedianFinder {
     private SortedSet<(int val, long indx)> leftPart;
     private SortedSet<(int val, long indx)> rightPart;
