71639a8164e66931a10f5e42f46461f3bbbc684c
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-06 11:37:43 +0530

    Time: 173 ms (12.43%), Space: 38.1 MB (28.94%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] CountBits(int n) {
+        int[] res = new int[n+1];
+        for(int i=1; i<= n; i++){
+            res[i] = res[i >> 1] + (i & 1);
+        }
+        return res;
+    }
+}
+        
