b55ef9d2b36c25e9d8cc2af903aec27ec3963d36
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-06 10:35:01 +0530

    Time: 172 ms (62.89%), Space: 49.4 MB (5.15%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.cs
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.cs
@@ -1,14 +1,42 @@
 public class Codec {
 
-    string delimeter = @"`^-^`";
+    string delimeter = @"$";
     // Encodes a list of strings to a single string.
     public string encode(IList<string> strs) {
-        return string.Join(delimeter, strs);
+        var str = string.Join("", strs);
+        StringBuilder sb = new StringBuilder();
+        foreach(var s in strs){
+            sb.Append(s.Length);
+            sb.Append(" ");
+        }
+        sb.Length--;
+        sb.Append(delimeter);
+        sb.Append(str);
+        return sb.ToString();
     }
 
     // Decodes a single string to a list of strings.
     public IList<string> decode(string s) {
-        return s.Split(delimeter).ToList();
+        var strs = new List<string>();
+        var pos = s.IndexOf(delimeter);
+        var subStr = s.Substring(0, pos);
+        var lengths = subStr.Split(" ");
+        int j=0, i = pos + 1;
+        
+        while(i < s.Length) {
+            int len = Convert.ToInt32(lengths[j++]);
+            if(len == 0)
+                strs.Add(string.Empty);
+            else
+                strs.Add(s.Substring(i, len));
+            i += len;
+        }
+        
+        while(j < lengths.Length){
+            strs.Add(string.Empty);
+            j++;
+        }
+        return strs;
     }
 }
 
