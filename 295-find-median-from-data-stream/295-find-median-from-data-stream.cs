eac2a503d7fd9ff7adc89cfaf06771ea9d49e954
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-06 11:02:05 +0530

    Time: 1132 ms (33.04%), Space: 93.2 MB (34.81%) - LeetHub

---
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cs
@@ -0,0 +1,72 @@
+/* 
+3 4 5 2 1 6 9 2 4 1 10
+
+1 1 2 2 3 4 |  4 5 6 9 10
+*/
+
+public class MedianFinder {
+    private SortedSet<(int val, long indx)> leftPart;
+    private SortedSet<(int val, long indx)> rightPart;
+    private long indx = 0;
+    public MedianFinder() {
+        leftPart = new SortedSet<(int, long)>();
+        rightPart = new SortedSet<(int, long)>();
+    }
+    
+    public void AddNum(int num) {
+        if(leftPart.Count == 0){
+            leftPart.Add((num, indx++));
+            return;
+        }
+        
+        var max = leftPart.Max;
+        var min = (rightPart.Count > 0) ? rightPart.Min : (int.MinValue, indx++);
+        
+        if(num < max.val)
+            leftPart.Add((num, indx++));
+        else
+            rightPart.Add((num, indx++));
+        
+        Balance();
+    }
+    
+    private void Balance(){
+        int leftLen = leftPart.Count;
+        int rightLen = rightPart.Count;
+        if(leftLen != rightLen){
+            var diff = Math.Abs(leftLen - rightLen);
+            if(diff == 1 && rightLen > leftLen) {
+                var min = PopMinOrMax(rightPart, true);
+                leftPart.Add(min);
+            } else if(diff == 2) {
+                var max = PopMinOrMax(leftPart, false);
+                rightPart.Add(max);
+            }
+        }
+    }
+    
+    private (int, long) PopMinOrMax(SortedSet<(int, long)> heap, bool isMin)
+    {
+        var minOrMax = isMin ? heap.Min : heap.Max;
+        heap.Remove(minOrMax);
+        return minOrMax;
+    }
+    
+    public double FindMedian() {
+        int leftLen = leftPart.Count;
+        int rightLen = rightPart.Count;
+        if(leftLen == 0)
+            return 0;
+        
+        if(leftLen == rightLen)
+            return (leftPart.Max.val + rightPart.Min.val) / 2.0;
+        return leftPart.Max.val*1.0;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.AddNum(num);
+ * double param_2 = obj.FindMedian();
+ */
