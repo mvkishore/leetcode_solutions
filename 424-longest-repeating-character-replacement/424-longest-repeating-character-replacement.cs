21fd2a055dcf0b3e7857579d64dc480847ecc46e
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-06 15:50:23 +0530

    Time: 80 ms (77.51%), Space: 36.2 MB (50.79%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+        int l = 0, r = 0, n = s.Length, maxCount = 0;
+        int[] count = new int[26];
+        int maxLen = 0;
+        while(r < n){
+            count[s[r] - 'A']++;
+            maxCount = Math.Max(maxCount, count[s[r] - 'A']);
+            r++;
+            
+            if((r - l) - maxCount > k){
+                count[s[l] - 'A']--;
+                l++;
+            }
+            maxLen = Math.Max(maxLen, r - l);
+        }
+        return maxLen;
+    }
+}
