1ba7182802120c2de997b8034b10c6cac10015bf
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-07 17:18:56 +0530

    Time: 160 ms (18.54%), Space: 38 MB (47.41%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cs
+++ 389-find-the-difference/389-find-the-difference.cs
@@ -1,16 +1,13 @@
 public class Solution {
     public char FindTheDifference(string s, string t) {
-        int[] counter = new int[26];
+        int ch = 0;
+        
         foreach(var c in s)
-            counter[c-'a']++;
+            ch ^= c;
         
         foreach(var c in t)
-            counter[c-'a']--;
+            ch ^= c;
         
-        for(int i=0;i<26; i++)
-            if(counter[i] == -1)
-                return (char) (i + 'a');
-        
-        return ' ';
+        return (char)ch;
     }
 }
