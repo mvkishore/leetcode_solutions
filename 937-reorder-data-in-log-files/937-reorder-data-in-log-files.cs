9058f763cd840e799ded04296962751e261218c5
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-07 10:55:29 +0530

    Time: 226 ms (43.35%), Space: 47.4 MB (5.59%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cs
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cs
@@ -1,18 +1,6 @@
 public class Solution {
     public string[] ReorderLogFiles(string[] logs) {
-        List<string> letter_logs = new List<string>();
-        List<string> digit_logs = new List<string>();
-        
-        foreach(var log in logs){
-            int idx = log.IndexOf(" ");
-            
-            if(idx < log.Length && char.IsDigit(log[idx + 1]))
-               digit_logs.Add(log);
-            else
-               letter_logs.Add(log);
-        }
-        
-        letter_logs.Sort((a, b) => {
+        return logs.OrderBy(x=>x, Comparer<string>.Create((a, b) => {
             int idx = a.IndexOf(" ");
             var aId = a.Substring(0, idx);
             var aLog = a.Substring(idx + 1);
@@ -20,22 +8,20 @@ public class Solution {
             idx = b.IndexOf(" ");
             var bId = b.Substring(0, idx);
             var bLog = b.Substring(idx + 1);
-            if(aLog == bLog)
-                return a.CompareTo(b);
             
-            return aLog.CompareTo(bLog);
-        });
-               
-        string[] res = new string[logs.Length];
-        int i=0;
-        for(int j=0; j<letter_logs.Count; j++){
-            res[i++] = letter_logs[j];
-        }
-        
-        for(int j=0; j<digit_logs.Count; j++){
-            res[i++] = digit_logs[j];
-        }
-        
-        return res;
+            var isADigitLog = char.IsDigit(aLog[0]);
+            var isBDigitLog = char.IsDigit(bLog[0]);
+            
+            if(!isADigitLog && !isBDigitLog){
+                if(aLog == bLog)
+                    return aId.CompareTo(bId);
+            
+                return aLog.CompareTo(bLog);
+            }else if(!isADigitLog && isBDigitLog){
+                return -1;
+            }else if(isADigitLog && !isBDigitLog) {
+                return 1;
+            }else return 0;
+        })).ToArray();
     }
 }
