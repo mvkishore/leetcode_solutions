0745bcfb3c4f57392dfdd2953e9959fcf3b343ee
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-11 12:39:56 +0530

    Time: 273 ms (25.73%), Space: 39.4 MB (85.55%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cs
@@ -0,0 +1,54 @@
+public class Solution {
+    public int ShortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.Length;
+        bool[,] visited = new bool[n, n];
+        
+        if(grid[0][0] != 0)
+            return -1;
+        
+        Queue<int[]> queue = new Queue<int[]>();
+        queue.Enqueue(new int[]{0, 0});
+        visited[0,0] = true;
+        
+        int len = 1;
+        while(queue.Count > 0)
+        {
+            int size = queue.Count;
+            for(int i=0; i<size; i++){
+                var cur = queue.Dequeue();
+                
+                if(cur[0] == n-1 && cur[1] == n-1)
+                    return len;
+                
+                foreach(var nei in GetNieghbors(cur[0], cur[1], n)){
+                    int nextRow = nei[0], nextCol = nei[1];
+                    if(!visited[nextRow, nextCol] && grid[nextRow][nextCol] == 0){
+                        if(nextRow == n -1 && nextCol == n - 1)
+                            return len + 1;
+                        
+                        visited[nextRow, nextCol] = true;
+                        queue.Enqueue(nei);
+                    }
+                }
+            }
+            len++;
+        }
+        
+        return -1;
+    }
+    private List<int[]> GetNieghbors(int row, int col, int size)
+    {
+        var dirs = new int[][] {new []{1, 0}, new []{-1, 0}
+                                , new []{0, 1}, new []{0, -1}
+                                , new []{1,-1}, new []{-1, 1}
+                                , new []{1, 1}, new []{-1, -1}};
+        var list = new List<int[]>();
+        
+        foreach(var dir in dirs){
+            int nextRow = row + dir[0], nextCol = col + dir[1];
+            if(nextRow >= 0 && nextRow < size && nextCol >= 0 && nextCol < size)
+                list.Add(new int[]{ nextRow, nextCol});
+        }
+        return list;
+    }
+}
