0fd4d61fd033c363a84b714b70e25c7c272734f9
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-12 21:41:02 +0530

    Time: 89 ms (65.12%), Space: 36.8 MB (53.70%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cs
+++ 13-roman-to-integer/13-roman-to-integer.cs
@@ -1,21 +1,21 @@
 public class Solution {
     public int RomanToInt(string s) {
-        Dictionary<string, int> symbolToNum = new Dictionary<string, int>(){
-            {"I", 1},
-            {"V", 5},
-            {"X", 10},
-            {"L", 50},
-            {"C", 100},
-            {"D", 500},
-            {"M", 1000}
+        Dictionary<char, int> symbolToNum = new Dictionary<char, int>(){
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
         };
         
-        string last = s.Substring(s.Length - 1);
+        char last = s[s.Length - 1];
         int prev = symbolToNum[last];
         int num = prev;
         
         for(int i=s.Length - 2; i>=0; i--){
-            last = s.Substring(i, 1);
+            last = s[i];
             int cur = symbolToNum[last];
             if(prev <= cur)
                 num += cur;
