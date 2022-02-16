5ed03eab7ccab425ddfd264a0c817523725e52d0
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-16 19:25:53 +0530

    Time: 190 ms (43.59%), Space: 40.9 MB (24.22%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cs
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cs
@@ -14,17 +14,17 @@
 public class Solution {
     public IList<int> InorderTraversal(TreeNode root) {
         IList<int> res = new List<int>();
-        InOrder(root, res);
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        
+        while(root != null || stack.Count > 0){
+            while(root != null){
+                stack.Push(root);
+                root = root.left;
+            }
+            root = stack.Pop();
+            res.Add(root.val);
+            root = root.right;
+        }
         return res;
     }
-    
-    private void InOrder(TreeNode root, IList<int> res)
-    {
-        if(root == null)
-            return;
-        InOrder(root.left, res);
-        res.Add(root.val);
-        InOrder(root.right, res);
-    }
-    
 }
