8d2b5c4457b78396b68ad68dfdc8329c8b5d3d21
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-16 08:25:06 +0530

    Time: 189 ms (19.78%), Space: 39.9 MB (37.36%) - LeetHub

--- 909-snakes-and-ladders/909-snakes-and-ladders.cs
+++ 909-snakes-and-ladders/909-snakes-and-ladders.cs
@@ -1,54 +1,47 @@
 public class Solution {
     public int SnakesAndLadders(int[][] board) {
         int n = board.Length;
-        Dictionary<int, int> steps = new Dictionary<int, int>();
-        steps.Add(1, 0);
+        int steps = 0;
+        bool[] visited = new bool[n * n + 1];
         Queue<int> queue = new Queue<int>();
+        visited[1] = true;
         queue.Enqueue(1);
-        
+            
         while(queue.Count > 0){
             int size = queue.Count;
-            for(int i=0; i < size; i++){
-                int cur = queue.Dequeue();
-                foreach(var next in GetNext(cur, board)){
-                    
+            for(int i=0; i<size; i++){
+                var cur = queue.Dequeue();
+                foreach(var next in GetNext(cur, board, n)){
                     if(next == n*n)
-                        return steps[cur] + 1;
-                    
-                    if(!steps.ContainsKey(next)){
-                        steps.Add(next, steps[cur] + 1);
+                        return steps + 1;
+                    if(!visited[next]) {
+                        visited[next] = true;
                         queue.Enqueue(next);
                     }
                 }
             }
+            steps++;
         }
         return -1;
     }
     
-    public List<int> GetNext(int cur, int[][] board)
+    private List<int> GetNext(int cur, int[][] board, int n)
     {
-        int n = board.Length;
-        var nextList = new List<int>();
-        
-        for(int i=1; i<= 6; i++) {
-            int nxt = Math.Min(cur + i, n *n);
+        List<int> nextList = new List<int>();
+        for(int i=1; i<=6; i++){
+            var next = Math.Min(cur + i, n *n);
             
-            int row = (nxt - 1) / n;
-            int col = (nxt - 1) % n;
-            
-            //Console.WriteLine($"{nxt} ==> {row}, {col} : Org");
+            int row = (next - 1) / n;
+            int col = (next - 1) % n;
             
             if(row % 2 != 0)
                 col = n - 1 - col;
             row = n - 1 - row;
             
-           // Console.WriteLine($"{nxt} ==> {row}, {col}");
-
             if(board[row][col] > 0)
-                nxt = board[row][col];
-            
-            nextList.Add(nxt);
-            if(nxt == n*n)
+                next = board[row][col];
+            nextList.Add(next);
+            if(next == n * n)
                 break;
         }
         return nextList;
