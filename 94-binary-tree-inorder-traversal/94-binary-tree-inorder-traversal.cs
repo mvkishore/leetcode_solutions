a986e27a496513c26385510c3959a0489e60e0f6
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-16 19:17:54 +0530

    Time: 176 ms (53.11%), Space: 40.8 MB (31.01%) - LeetHub

---
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cs
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<int> InorderTraversal(TreeNode root) {
+        IList<int> res = new List<int>();
+        InOrder(root, res);
+        return res;
+    }
+    
+    private void InOrder(TreeNode root, IList<int> res)
+    {
+        if(root == null)
+            return;
+        InOrder(root.left, res);
+        res.Add(root.val);
+        InOrder(root.right, res);
+    }
+    
+}
