4f623b291e389957dc3e58e10067e98b25b5cd33
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-17 21:53:00 +0530

    Time: 113 ms (60.10%), Space: 39.6 MB (55.75%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cs
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cs
@@ -22,10 +22,10 @@ public class Solution {
         if(root2 == null)
             return root1;
         
-        TreeNode root = new TreeNode(root1.val + root2.val);
-        root.left = MergeTrees(root1.left, root2.left);
-        root.right = MergeTrees(root1.right, root2.right);
+        root1.val += root2.val;
+        root1.left = MergeTrees(root1.left, root2.left);
+        root1.right = MergeTrees(root1.right, root2.right);
         
-        return root;
+        return root1;
     }
 }
