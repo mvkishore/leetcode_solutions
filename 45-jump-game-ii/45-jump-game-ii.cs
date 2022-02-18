519eec799df97c4e0030c4a99221a2618a6d7f3f
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-18 10:00:10 +0530

    Time: 148 ms (58.19%), Space: 39.2 MB (67.03%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cs
+++ 45-jump-game-ii/45-jump-game-ii.cs
@@ -1,33 +1,15 @@
 public class Solution {
     public int Jump(int[] nums) {
         int n = nums.Length;
-        if(n == 1)
-            return 0;
-        
-        bool[] visited = new bool[n];
-        Queue<int> queue = new Queue<int>();
-        
-        int steps = 0;
-        queue.Enqueue(0);
-        visited[0] = true;
-        
-        while(queue.Count > 0){
-            int size = queue.Count;
-            for(int i=0; i<size; i++){
-                var cur = queue.Dequeue();
-                for(int j=1; j<=nums[cur]; j++){
-                    var nextPos = cur + j;
-                    if(nextPos >= n - 1)
-                        return steps + 1;
-                    if(!visited[nextPos]){
-                        visited[nextPos] = true;
-                        queue.Enqueue(nextPos);
-                    }
-                }
+        int maxJump = 0, curMaxJump = 0, jump = 0;
+        for(int i=0; i < n-1; i++){
+            maxJump = Math.Max(maxJump, i + nums[i]);
+            
+            if(i == curMaxJump){
+                jump++;
+                curMaxJump = maxJump;
             }
-            steps++;
         }
-        return 0;
-        
+        return jump;
     }
 }
