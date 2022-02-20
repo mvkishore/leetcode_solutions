73dcd290e82a2312d2d1281bb35c1bb3a8d3a43d
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-20 09:33:41 +0530

    Time: 185 ms (66.07%), Space: 46.4 MB (11.91%) - LeetHub

---
+++ 986-interval-list-intersections/986-interval-list-intersections.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int[][] IntervalIntersection(int[][] firstList, int[][] secondList) {
+        int left = 0, right = 0;
+        int m = firstList.Length, n = secondList.Length;
+        List<int[]> res = new List<int[]>();
+        
+        while(left < m && right < n){
+            if(IsOverlap(firstList[left], secondList[right])){
+                var merge = Merge(firstList[left], secondList[right]);
+                res.Add(merge);
+                if(firstList[left][1] <= merge[1])
+                    left++;
+                else
+                    right++;
+            }else if(firstList[left][1] < secondList[right][1]){
+                left++;
+            }else right++;
+        }
+        return res.ToArray();
+    }
+    
+    private bool IsOverlap(int[] a, int[] b){
+        return a[0] <= b[1] && b[0] <= a[1];
+    }
+    
+    private int[] Merge(int[] a, int[] b){
+        return new int[]{ Math.Max(a[0], b[0]), Math.Min(b[1], a[1])};
+    }
+}
