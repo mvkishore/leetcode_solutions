2c79c3627c47be5b09d7cb8c2b171d13e0fff304
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-21 09:27:23 +0530

    Time: 111 ms (79.08%), Space: 36.5 MB (68.84%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cs
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cs
@@ -1,27 +1,26 @@
 public class Solution {
     public string LongestPalindrome(string s) {
-        int n = s.Length, maxLen = -1, startIndex = -1;
-        
+        int n = s.Length, maxLen = 0, startIndx = -1;
         for(int i=0; i<n; i++){
-            int oddLen = GetPalindromeLen(s, i, i);
-            int eveLen = GetPalindromeLen(s, i, i + 1);
+            int oddLength = GetPalindrome(s, i, i);
+            int evenLength = GetPalindrome(s, i, i+1);
             
-            int len = Math.Max(oddLen, eveLen);
-            if(maxLen < len){
+            int len = Math.Max(oddLength, evenLength);
+            if(len > maxLen){
                 maxLen = len;
-                startIndex = i - (len - 1) / 2;
+                startIndx = i - (len - 1) / 2;
             }
         }
-        return s.Substring(startIndex, maxLen);
+        return s.Substring(startIndx, maxLen);
     }
     
-    private int GetPalindromeLen(string s, int l, int r)
+    private int GetPalindrome(string s, int left, int right)
     {
         int n = s.Length;
-        while(l >= 0 && r < n && s[l] == s[r]){
-            l--;
-            r++;
+        while(left >= 0 && right < n && s[left] == s[right]){
+            left--;
+            right++;
         }
-        return r - l - 1;
+        return right - left - 1;
     }
 }
