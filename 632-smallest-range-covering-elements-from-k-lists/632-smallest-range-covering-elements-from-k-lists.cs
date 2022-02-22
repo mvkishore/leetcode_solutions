70861f64e7d3dd53e84c590f87a817d25f32c2ef
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-22 06:57:15 +0530

    Time: 450 ms (22.73%), Space: 48.4 MB (6.82%) - LeetHub

---
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int[] SmallestRange(IList<IList<int>> nums) {
+        int m = nums.Count, n = nums[0].Count;
+        
+        SortedSet<(int val, int i, int j)> heap = new SortedSet<(int val, int i, int j)>();
+        
+        for(int i=0; i<m; i++)
+            heap.Add((nums[i][0], i, 0));
+        int rangeMin = 0, rangeMax = int.MaxValue;
+        
+        while(heap.Count > 0){
+            var min = heap.Min;
+            var max = heap.Max;
+            if(rangeMax - rangeMin > max.val - min.val){
+                rangeMin  = min.val;
+                rangeMax = max.val;
+            }
+            heap.Remove(min);
+            var nextI = min.i;
+            var nextJ = min.j + 1;
+            if(nextJ == nums[nextI].Count)
+                return new int[] {rangeMin, rangeMax};
+            
+            heap.Add((nums[nextI][nextJ], nextI, nextJ));
+        }
+        return new int[0];
+    }
+}
