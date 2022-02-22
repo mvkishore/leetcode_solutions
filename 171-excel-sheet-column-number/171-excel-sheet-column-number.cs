6aa4bbf8b80fce7e26a446db999174b9ce8ff3e7
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-22 07:05:45 +0530

    Time: 68 ms (82.63%), Space: 38.6 MB (12.75%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cs
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cs
@@ -1,16 +1,12 @@
-/*ZY
-
-A
-AA
-26+1 =27
-26*26 + 25 => 
-*/
 public class Solution {
     public int TitleToNumber(string columnTitle) {
-        int num = 0;
-        for(int i=0; i<columnTitle.Length; i++){
-            num = num * 26 + (columnTitle[i] - 'A' + 1);
+        int n = columnTitle.Length, i = 0;
+        int res = 0;
+        while(i < n){
+            var c = columnTitle[i];
+            res = res * 26 + (c - 'A') + 1;
+            i++;            
         }
-        return num;
+        return res;
     }
 }
