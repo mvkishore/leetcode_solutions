17d8b0f50df5f7ef956e77f67064c0ef8c26b29e
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-22 09:19:10 +0530

    Time: 337 ms (38.24%), Space: 48.5 MB (32.70%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cs
+++ 53-maximum-subarray/53-maximum-subarray.cs
@@ -1,11 +1,36 @@
 public class Solution {
     public int MaxSubArray(int[] nums) {
-        int maxSum = int.MinValue, n = nums.Length;
-        int sum = 0;
-        for(int i=0; i<n; i++){
-            sum = Math.Max(nums[i], nums[i] + sum);
-            maxSum = Math.Max(sum, maxSum);
+        return MaxSubArray(nums, 0, nums.Length - 1);
+    }
+    
+    private int MaxSubArray(int[] nums, int left, int right)
+    {
+        if(left > right){
+            return int.MinValue;
         }
-        return maxSum;
+        
+        int mid = left + (right - left ) / 2;
+        int sum = 0;
+        int maxLeftSum = 0;
+        
+        for(int i=mid - 1; i>= left; i--){
+            sum += nums[i];
+            maxLeftSum = Math.Max(maxLeftSum, sum);
+        }
+        
+        sum = 0;
+        int maxRightSum = 0;
+        
+        for(int i=mid+1; i <= right; i++){
+            sum += nums[i];
+            maxRightSum = Math.Max(maxRightSum, sum);
+        }
+        
+        int combinedSum = maxLeftSum + nums[mid] + maxRightSum;
+        
+        int leftSide = MaxSubArray(nums, left, mid -1);
+        int rightSide = MaxSubArray(nums, mid + 1, right);
+        
+        return Math.Max(combinedSum, Math.Max(leftSide, rightSide));
     }
 }
