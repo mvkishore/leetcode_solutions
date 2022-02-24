635e794144ee30380a61e4931cd9293f3bb6cc6a
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-24 21:02:59 +0530

    Time: 36 ms (59.82%), Space: 25.3 MB (77.42%) - LeetHub

--- 62-unique-paths/62-unique-paths.cs
+++ 62-unique-paths/62-unique-paths.cs
@@ -1,13 +1,12 @@
 public class Solution {
     public int UniquePaths(int m, int n) {
         int[,] dp = new int[m, n];
+        dp[0, 0] = 1;
         
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(i == 0 && j == 0)
-                    dp[i,j] = 1;
-                dp[i,j] += i == 0 ? 0 : dp[i - 1, j];
-                dp[i,j] += j == 0 ? 0 : dp[i, j-1];
+                if(i != 0 || j != 0)
+                    dp[i, j] = (i > 0 ? dp[i - 1, j] : 0) + (j > 0 ? dp[i, j-1] : 0);
             }
         }
         return dp[m-1, n-1];
