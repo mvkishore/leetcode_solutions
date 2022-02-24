6a99ddaeedfbeabc4a02e6af0fd2eccf0adca278
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-24 20:35:20 +0530

    Time: 291 ms (44.67%), Space: 42.3 MB (56.04%) - LeetHub

--- 55-jump-game/55-jump-game.cs
+++ 55-jump-game/55-jump-game.cs
@@ -1,10 +1,22 @@
 public class Solution {
     public bool CanJump(int[] nums) {
+        int curMaxIndx = 0, nextMaxIndx = 0;
         int n = nums.Length;
-        int max = 0;
-        for(int i=0; i <= max; i++){
-            max = Math.Max(max, i + nums[i]);
-            if(max >= n - 1) return true;
+        
+        curMaxIndx = nums[0];
+        
+        if(n == 1)
+            return true;
+        
+        for(int i=1; i < n; i++){
+            nextMaxIndx = Math.Max(nextMaxIndx, i + nums[i]);
+            
+            if(curMaxIndx >= n - 1){
+                return true;
+            } else if(curMaxIndx == i){
+                curMaxIndx = nextMaxIndx;
+                nextMaxIndx = 0;
+            }
         }
         return false;
     }
