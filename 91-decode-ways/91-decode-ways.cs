7b42a905be91a867fd512d8a78e9a1a16a275efd
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-25 09:34:04 +0530

    Time: 66 ms (86.74%), Space: 35.9 MB (34.94%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public int NumDecodings(string s) {
+        int?[] cache = new int?[s.Length];
+        
+        return NumDecodings(s, 0, cache).Value;
+    }
+    
+    private int? NumDecodings(string s, int index, int?[] cache){
+        if(index > s.Length)
+            return 0;
+        
+        if(index == s.Length)
+            return 1;
+        
+        if(cache[index] != null)
+            return cache[index];
+        
+        if(s[index] == '0')
+            return cache[index] = 0;
+        
+        if(index == s.Length - 1)
+            return cache[index] = 1;
+        
+        int? opt1 = NumDecodings(s, index + 1, cache);
+        int? opt2 = 0;
+        int num = Convert.ToInt32(s.Substring(index, 2));
+        if(num >= 10 && num <= 26)
+            opt2 = NumDecodings(s, index + 2, cache);
+        
+        return cache[index] = opt1 + opt2;
+    }
+}
+
+//--> 1 1 106  --> 4: 1, 3: 0, 2: 1, 1: 1, 0: 2 <- anse
+    
