5fa87f72d33bb8faa01332c253dd09ba21358638
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-25 07:44:47 +0530

    Time: 152 ms (80.31%), Space: 46.1 MB (36.39%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cs
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cs
@@ -2,18 +2,18 @@ public class Solution {
     public void SetZeroes(int[][] matrix) {
         int rows = matrix.Length, cols = matrix[0].Length;
         bool isColReset = false;
-        for(int row = 0; row< rows; row++){
+        
+        for(int row=0; row < rows; row++){
             if(matrix[row][0] == 0)
                 isColReset = true;
             
-            for(int col=1; col< cols; col++){
-                
+            for(int col = 1; col< cols; col++){
                 if(matrix[row][col] == 0)
-                    matrix[0][col] = matrix[row][0] = 0;
+                    matrix[row][0] = matrix[0][col] = 0;
             }
         }
         
-        for(int row=1; row < rows; row++){
+        for(int row=1; row<rows; row++){
             for(int col=1; col < cols; col++){
                 if(matrix[row][0] == 0 || matrix[0][col] == 0)
                     matrix[row][col] = 0;
@@ -21,7 +21,7 @@ public class Solution {
         }
         
         if(matrix[0][0] == 0){
-            for(int col=0; col<cols; col++)
+            for(int col=1; col < cols; col++)
                 matrix[0][col] = 0;
         }
         
