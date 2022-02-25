3d7047b495aaeb999ca9b46327f2d52abbe3a703
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-25 21:39:05 +0530

    Time: 122 ms (64.32%), Space: 38.7 MB (37.28%) - LeetHub

--- 100-same-tree/100-same-tree.cs
+++ 100-same-tree/100-same-tree.cs
@@ -15,13 +15,10 @@ public class Solution {
     public bool IsSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null)
             return true;
-        
         if(p == null)
             return false;
-        
         if(q == null)
             return false;
-        
         return p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
     }
 }
