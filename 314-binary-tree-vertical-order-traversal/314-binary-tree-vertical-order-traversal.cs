ab62d76d476335d547a3c3c13f63e1cfdc593656
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-26 16:31:47 +0530

    Time: 199 ms (54.59%), Space: 42.2 MB (32.75%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.cs
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.cs
@@ -10,55 +10,40 @@
  *         this.right = right;
  *     }
  * }
- 
- 
-            3                       0
-        9       8
-    4       01      7
- 
- 
- 
- 
  */
 public class Solution {
     public IList<IList<int>> VerticalOrder(TreeNode root) {
+        int left = int.MaxValue, right = int.MinValue;
+        IList<IList<int>> results = new List<IList<int>>();
         if(root == null)
-            return new List<IList<int>>();
+            return results;
         
-        Dictionary<int, List<int>> cols = new Dictionary<int, List<int>>();
-        Queue<(TreeNode node, int col)> queue = new Queue<(TreeNode node, int col)>();
+        Dictionary<int, IList<int>> verticalLevels = new Dictionary<int, IList<int>>();
+        Queue<(TreeNode node, int level)> queue = new Queue<(TreeNode node, int level)>();
+        
         queue.Enqueue((root, 0));
-        
-        int minCol = 0, maxCol = 0;
         while(queue.Count > 0){
-            int size = queue.Count;
+            var size = queue.Count;
             
-            for(int i= 0; i < size ; i++){
+            for(int i=0; i<size; i++){
                 var cur = queue.Dequeue();
-                
-                int col = cur.col;
-                int val = cur.node.val;
-                
-                if(!cols.ContainsKey(col))
-                    cols.Add(col, new List<int>());
-                
-                cols[col].Add(val);
+                if(!verticalLevels.ContainsKey(cur.level))
+                    verticalLevels.Add(cur.level, new List<int>());
+                verticalLevels[cur.level].Add(cur.node.val);
                 
                 if(cur.node.left != null)
-                    queue.Enqueue((cur.node.left, col - 1));
-                
+                    queue.Enqueue((cur.node.left, cur.level - 1));
                 if(cur.node.right != null)
-                    queue.Enqueue((cur.node.right, col + 1));
-                                  
-                minCol = Math.Min(minCol, col);
-                maxCol = Math.Max(maxCol, col);
+                    queue.Enqueue((cur.node.right, cur.level + 1));
+                
+                left = Math.Min(cur.level, left);
+                right = Math.Max(cur.level, right);
             }
         }
         
-        IList<IList<int>> res = new List<IList<int>>();
-        for(int i=minCol; i<= maxCol; i++)
-            res.Add(cols[i]);
-        
-        return res;
+        for(int i=left; i<= right; i++){
+            results.Add(verticalLevels[i]);
+        }
+        return results;
     }
 }
