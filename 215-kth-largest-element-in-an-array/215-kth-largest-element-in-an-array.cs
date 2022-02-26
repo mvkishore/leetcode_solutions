425c7d609285372338a400801055ec1a18341992
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-26 09:01:52 +0530

    Time: 126 ms (62.73%), Space: 38.3 MB (51.65%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cs
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cs
@@ -1,18 +1,12 @@
 public class Solution {
     public int FindKthLargest(int[] nums, int k) {
-        SortedSet<(int ele, int idx)> heap = new SortedSet<(int ele, int idx)>();
-        int i=0;
-        foreach(var num in nums){
-            if(heap.Count == k){
-                if(heap.Min.ele < num)
-                    heap.Remove(heap.Min);
-                else
-                    continue;
-            }
-            heap.Add((num, i++));
+        PriorityQueue<int, int> queue = new PriorityQueue<int, int>();
+        int n = nums.Length;
+        for(int i=0; i<n; i++){
+            queue.Enqueue(nums[i], nums[i]);
+            if(queue.Count > k)
+                queue.Dequeue();
         }
-        return heap.Min.ele;
+        return queue.Dequeue();
     }
 }
-
-//[6,6,5,5,6,6,3,2,1,3]
