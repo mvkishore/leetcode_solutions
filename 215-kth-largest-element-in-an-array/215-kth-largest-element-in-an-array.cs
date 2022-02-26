e21c26ff86133e79fd79ff2480aaad0483d25348
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-26 09:22:15 +0530

    Time: 142 ms (48.35%), Space: 38.2 MB (63.16%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cs
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cs
@@ -1,12 +1,47 @@
 public class Solution {
     public int FindKthLargest(int[] nums, int k) {
-        PriorityQueue<int, int> queue = new PriorityQueue<int, int>();
         int n = nums.Length;
-        for(int i=0; i<n; i++){
-            queue.Enqueue(nums[i], nums[i]);
-            if(queue.Count > k)
-                queue.Dequeue();
+        
+        return QuickSelect(nums, 0, n-1, n - k);
+    }
+    
+    private int QuickSelect(int[] nums, int left, int right, int k)
+    {
+        if(left == right)
+            return nums[left];
+        
+        int n = nums.Length;
+        
+        var pos = Partition(nums, left, right);
+        
+        if(pos == k)
+            return nums[pos];
+        if(pos < k){
+            return QuickSelect(nums, pos + 1, right, k);
         }
-        return queue.Dequeue();
+        return QuickSelect(nums, left, pos - 1, k);
+    }
+    
+    private int Partition(int[] nums, int left, int right)
+    {
+        int pivot = nums[right];
+        
+        int i = left;
+        for(int j = left; j < right; j++){
+            if(nums[j] < pivot){
+                Swap(nums, j, i++);
+            }
+        }
+        
+        Swap(nums, i, right);
+        return i;
+    }
+    
+    private void Swap(int[] nums, int i, int j){
+        if(i != j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
     }
 }
