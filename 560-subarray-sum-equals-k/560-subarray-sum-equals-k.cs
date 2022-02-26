23085466d9ccba6345d566ac64c67be47d8c7fde
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-26 07:40:34 +0530

    Time: 158 ms (76.31%), Space: 42.9 MB (49.71%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cs
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cs
@@ -1,16 +1,14 @@
 public class Solution {
     public int SubarraySum(int[] nums, int k) {
-        int n = nums.Length, sum = 0, count = 0;
         Dictionary<int, int> seenSums = new Dictionary<int, int>();
+        int sum = 0, n = nums.Length, count = 0;
         seenSums.Add(0, 1);
         for(int i=0; i<n; i++){
             sum += nums[i];
             if(seenSums.ContainsKey(sum - k))
                 count += seenSums[sum - k];
-            
             if(!seenSums.ContainsKey(sum))
                 seenSums.Add(sum, 0);
-            
             seenSums[sum]++;
         }
         return count;
