715d99106971d0e93a61827e79365d4951ff1655
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-26 20:54:33 +0530

    Time: 104 ms (65.46%), Space: 37.4 MB (52.37%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cs
+++ 162-find-peak-element/162-find-peak-element.cs
@@ -1,16 +1,16 @@
 public class Solution {
     public int FindPeakElement(int[] nums) {
-        int n = nums.Length, low = 0, hi = n -1;
-        
-        while(low < hi){
-            int mid = low + (hi - low) / 2;
+        int n = nums.Length, lo = 0, hi = n -1;
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
             
             if(mid > 0 && nums[mid - 1] > nums[mid])
                 hi = mid - 1;
-            else if(mid < n && nums[mid] < nums[mid + 1])
-                low = mid + 1;
+            else if(mid < n - 1 && nums[mid + 1] > nums[mid])
+                lo = mid + 1;
             else return mid;
         }
-        return low;
+        
+        return lo;
     }
 }
