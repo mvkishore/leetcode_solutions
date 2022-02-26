01b0aa847dd1c342d2c0b45e98974a7f81f7b93d
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-26 08:09:10 +0530

    Time: 116 ms (98.28%), Space: 43.7 MB (5.91%) - LeetHub

--- 31-next-permutation/31-next-permutation.cs
+++ 31-next-permutation/31-next-permutation.cs
@@ -1,32 +1,39 @@
+//2 3 4 1 6 9 8 7 2  => {2, 7, 6}
+
+//2 3 4 1 7 2 6
+
+
+//7 6 4 3 2 2 1
+
 public class Solution {
     public void NextPermutation(int[] nums) {
         int n = nums.Length;
-        
-        int explorePos = n - 2;
-        while(explorePos >= 0 && nums[explorePos + 1] <= nums[explorePos])
-            explorePos--;
-        
-        if(explorePos >= 0){
-            int nextPos = explorePos + 1;
-            while(nextPos < n && nums[nextPos] > nums[explorePos])
-                nextPos++;
-            
-            Swap(nums, explorePos, nextPos - 1);
-        }
-        Reverse(nums, explorePos + 1, n-1);
-    }
-    
-    private void Reverse(int[] nums, int start, int end)
-    {
-        while(start < end){
-            Swap(nums, start++, end--);
+        int i = n - 1;
+        while(i > 0 && nums[i - 1] >= nums[i])
+            i--;
+        if(i == 0)
+            Array.Reverse(nums);
+        else {
+            i = i - 1;
+            int j = i + 1;
+            while(j < n && nums[j] > nums[i])
+                j++;
+            Swap(nums, j-1, i);
+            Reverse(nums, i+ 1, n - 1);
         }
     }
     
-    private void Swap(int[] nums, int i, int j)
-    {
+    private void Swap(int[] nums, int i, int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+    
+    private void Reverse(int[] nums, int s, int e){
+        while(s < e){
+            Swap(nums, s, e);
+            s++;
+            e--;
+        }
     }
 }
