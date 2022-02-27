9febb95773251e7a812e62683275f74cebc7b019
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-27 12:43:07 +0530

    Time: 204 ms (62.72%), Space: 43.1 MB (14.22%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cs
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cs
@@ -1,53 +1,61 @@
 public class Solution {
     public int ShortestPathBinaryMatrix(int[][] grid) {
-        int n = grid.Length;
-        
         if(grid[0][0] != 0)
             return -1;
         
+        int n = grid.Length;
         Queue<int[]> queue = new Queue<int[]>();
-        queue.Enqueue(new int[]{0, 0});
-        grid[0][0] = 1;
+        bool[,] visited = new bool[n, n];
         
-        int len = 1;
-        while(queue.Count > 0)
-        {
+        queue.Enqueue(new []{0, 0});
+        visited[0, 0] = true;
+        int path = 1;
+        
+        while(queue.Count > 0){
             int size = queue.Count;
             for(int i=0; i<size; i++){
                 var cur = queue.Dequeue();
                 
-                if(cur[0] == n-1 && cur[1] == n-1)
-                    return len;
+                if(cur[0] == n - 1 && cur[1] == n - 1)
+                    return path;
                 
-                foreach(var nei in GetNieghbors(cur[0], cur[1], n)){
+                foreach(var nei in GetNeibors(cur, n))
+                {
                     int nextRow = nei[0], nextCol = nei[1];
-                    if(grid[nextRow][nextCol] == 0){
-                        if(nextRow == n -1 && nextCol == n - 1)
-                            return len + 1;
-                        
-                        grid[nextRow][nextCol] = 1;
-                        queue.Enqueue(nei);
+                    if(nextRow == n - 1 && nextCol == n - 1 && grid[nextRow][nextCol] != 1)
+                        return 1 + path;
+                    
+                    if(!visited[nextRow, nextCol] && grid[nextRow][nextCol] != 1){
+                        visited[nextRow, nextCol] = true;
+                        queue.Enqueue(new[]{nextRow, nextCol});
                     }
                 }
             }
-            len++;
+            path++;
         }
         
         return -1;
     }
-    private List<int[]> GetNieghbors(int row, int col, int size)
-    {
-        var dirs = new int[][] {new []{1, 0}, new []{-1, 0}
-                                , new []{0, 1}, new []{0, -1}
-                                , new []{1,-1}, new []{-1, 1}
-                                , new []{1, 1}, new []{-1, -1}};
-        var list = new List<int[]>();
-        
+    
+    private IList<int[]> GetNeibors(int[] cur, int n){
+        var dirs = new int[][] {
+            new []{0, 1},
+            new []{1, 0},
+            new []{0, -1},
+            new []{-1, 0},
+            new []{1, 1},
+            new []{-1, -1},
+            new []{1, -1},
+            new []{-1, 1},
+        };
+        var neibors = new List<int[]>();
         foreach(var dir in dirs){
-            int nextRow = row + dir[0], nextCol = col + dir[1];
-            if(nextRow >= 0 && nextRow < size && nextCol >= 0 && nextCol < size)
-                list.Add(new int[]{ nextRow, nextCol});
+            int nextRow = cur[0] + dir[0];
+            int nextCol = cur[1] + dir[1];
+            
+            if(nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < n)
+                neibors.Add(new []{ nextRow, nextCol });
         }
-        return list;
+        return neibors;
     }
 }
