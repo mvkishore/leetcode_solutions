6495b86202fdb40201bb8dc07de7e526dc613de9
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-27 20:14:43 +0530

    Time: 115 ms (75.60%), Space: 38.3 MB (64.60%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.cs
+++ 269-alien-dictionary/269-alien-dictionary.cs
@@ -1,54 +1,65 @@
 public class Solution {
     public string AlienOrder(string[] words) {
-        Dictionary<char, List<char>> orderGraph = new Dictionary<char, List<char>>();
-        Dictionary<char, int> indegrees = new Dictionary<char, int>();
+        int n = words.Length;
+        Dictionary<char, HashSet<char>> depMap = new Dictionary<char, HashSet<char>>();
+        Dictionary<char, int> inDegree = new Dictionary<char, int>();
         
-        foreach(var word in words){
-            foreach(var c in word){
-                if(!orderGraph.ContainsKey(c)){
-                    orderGraph.Add(c, new List<char>());
-                    indegrees.Add(c, 0);
-                }
+        foreach(var word in words)
+        {
+            foreach(var c in word)
+            {
+                if(!inDegree.ContainsKey(c))
+                    inDegree.Add(c, 0);
             }
         }
         
-        for(int i=0; i < words.Length - 1; i++){
-            var word1 = words[i];
-            var word2 = words[i+1];
-            
-            if(word1 == word2) continue;
-            
-            if(word1.Length > word2.Length && word1.StartsWith(word2))
-                return string.Empty;
-            
-            for(int j=0; j< Math.Min(word1.Length, word2.Length); j++)
-            {
-                if(word1[j] != word2[j]){
-                    if(!orderGraph[word1[j]].Contains(word2[j])){
-                        orderGraph[word1[j]].Add(word2[j]);
-                        indegrees[word2[j]]++;
-                    }
+        var prev = words[0];
+        for(int i=1; i < n; i++){
+            var cur = words[i];
+            bool noMatch = true;
+            for(int j=0; j < Math.Min(prev.Length, cur.Length); j++){
+                if(prev[j] != cur[j]){
+                    var u = prev[j];
+                    var v = cur[j];
+                    if(!depMap.ContainsKey(u))
+                        depMap.Add(u, new HashSet<char>());
+                    
+                    if(depMap[u].Add(v))
+                        inDegree[v]++;
+                    noMatch = false;
                     break;
                 }
             }
+            if(noMatch && cur.Length < prev.Length)
+                return string.Empty;
+            prev = cur;
         }
         
         StringBuilder sb = new StringBuilder();
         Queue<char> queue = new Queue<char>();
         
-        foreach(var c in indegrees.Where(x => x.Value == 0).Select(x=> x.Key))
-            queue.Enqueue(c);
+        foreach(var inDegreeChar in inDegree){
+            if(inDegreeChar.Value == 0)
+                queue.Enqueue(inDegreeChar.Key);
+        }
         
         while(queue.Count > 0){
-            var cur = queue.Dequeue();
-            sb.Append(cur);
-            foreach(var nextChars in orderGraph[cur]){
-                indegrees[nextChars]--;
-                if(indegrees[nextChars] == 0)
-                    queue.Enqueue(nextChars);
+            var size = queue.Count;
+            for(int i=0; i<size; i++){
+                var cur = queue.Dequeue();
+                sb.Append(cur);
+                if(depMap.ContainsKey(cur)){
+                    foreach(var nei in depMap[cur]){
+                        inDegree[nei]--;
+                        if(inDegree[nei] == 0)
+                            queue.Enqueue(nei);
+                    }
+                }
             }
         }
         
-        return sb.Length != indegrees.Count ? string.Empty: sb.ToString();
+        if(sb.Length == inDegree.Count) return sb.ToString();
+        
+        return string.Empty;
     }
 }
