771765088fe8568d56339250bc128cf331754e1d
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-27 08:53:40 +0530

    Time: 510 ms (41.27%), Space: 46.6 MB (33.27%) - LeetHub

---
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.cs
@@ -0,0 +1,28 @@
+public class SparseVector {
+    private Dictionary<int, int> values;
+    public SparseVector(int[] nums) {
+        values = new Dictionary<int, int>();
+        for(int i=0; i<nums.Length; i++){
+            if(nums[i] > 0)
+                values[i] = nums[i];
+        }
+    }
+    
+    public Dictionary<int, int> Values => values;
+    
+    // Return the dotProduct of two sparse vectors
+    public int DotProduct(SparseVector vec) {
+        int res = 0;
+        foreach(var key in values.Keys){
+            if(vec.Values.ContainsKey(key)){
+                res += (vec.Values[key] * values[key]);
+            }
+        }
+        return res;
+    }
+}
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1 = new SparseVector(nums1);
+// SparseVector v2 = new SparseVector(nums2);
+// int ans = v1.DotProduct(v2);
