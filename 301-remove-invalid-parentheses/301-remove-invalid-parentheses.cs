e36a4271b311c85090f031593b5b0260ed41f48b
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-28 19:07:36 +0530

    Time: 292 ms (61.51%), Space: 48 MB (10.79%) - LeetHub

---
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cs
@@ -0,0 +1,47 @@
+public class Solution {
+    public IList<string> RemoveInvalidParentheses(string s) {
+        IList<string> res = new List<string>();
+        Queue<string> queue = new Queue<string>();
+        HashSet<string> visited = new HashSet<string>();
+        queue.Enqueue(s);
+        visited.Add(s);
+        while(queue.Count > 0){
+            var size = queue.Count;
+            
+            for(int i=0; i<size; i++){
+                var cur = queue.Dequeue();
+                
+                if(IsValid(cur)){
+                    res.Add(cur);
+                }
+                
+                if(res.Count == 0){
+                   for(int j=0; j < cur.Length; j++) {
+                       if(cur[j] == '(' || cur[j] == ')')
+                       {
+                           var next = cur.Substring(0, j) + cur.Substring(j+1, cur.Length - j - 1);
+                           if(!visited.Contains(next)){
+                               visited.Add(next);
+                               queue.Enqueue(next);
+                           }
+                       }
+                   }
+                }
+            }
+        }
+        return res;
+    }
+    
+    private bool IsValid(string s){
+        int openCount = 0;
+        foreach(var c in s){
+            if(c == '(')
+                openCount++;
+            else if(c == ')')
+                openCount--;
+            if(openCount < 0)
+                return false;
+        }
+        return openCount == 0;
+    }
+}
