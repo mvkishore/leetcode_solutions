a2adb8aff76f019d8c8b336341ba6d7ad846641c
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-28 20:26:12 +0530

    Time: 1149 ms (41.21%), Space: 39.7 MB (64.19%) - LeetHub

--- 317-shortest-distance-from-all-buildings/317-shortest-distance-from-all-buildings.cs
+++ 317-shortest-distance-from-all-buildings/317-shortest-distance-from-all-buildings.cs
@@ -1,21 +1,20 @@
 public class Solution {
     public int ShortestDistance(int[][] grid) {
-        int houses = 0;
-        int rows = grid.Length;
-        int cols = grid[0].Length;
+        int rows = grid.Length, cols = grid[0].Length;
         int minDist = int.MaxValue;
+        int houses = 0;
         
-        for(int row=0; row<rows; row++){
-            for(int col=0; col<cols; col++){
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
                 if(grid[row][col] == 1)
                     houses++;
             }
         }
         
-        for(int row=0; row<rows; row++){
-            for(int col=0; col<cols; col++){
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
                 if(grid[row][col] == 0){
-                    minDist = Math.Min(minDist, GetDistance(grid, row, col, houses));
+                    minDist = Math.Min(minDist, GetMinDistance(grid, row, col, houses));
                 }
             }
         }
@@ -23,61 +22,56 @@ public class Solution {
         if(minDist == int.MaxValue) return -1;
         
         return minDist;
-        
     }
     
-    private int GetDistance(int[][] grid, int row, int col, int houses){
-        int rows = grid.Length;
-        int cols = grid[0].Length;
-        var dirs = new int[][] { new []{1, 0}, new []{-1, 0},new []{0, 1}, new []{0, -1}};
-        int dist = 0;
-        int totalDistance = 0;
-        int reachedHouses = 0;
+    private int GetMinDistance(int[][] grid, int row, int col, int houses)
+    {
+        int rows = grid.Length, cols = grid[0].Length;
+        int[][] dirs = new int[][]{new []{0, 1}, new[]{0, -1}, new []{1, 0}, new[]{-1, 0}};
+        int visitedHouses = 0, distance = 0, totalDist = 0;
         
-        bool[,] visited = new bool[rows, cols];
         Queue<int[]> queue = new Queue<int[]>();
-        
+        bool[,] visited = new bool[rows, cols];
         visited[row, col] = true;
-        queue.Enqueue(new int[]{ row, col});
-
+        queue.Enqueue(new []{row, col});
+        
         while(queue.Count > 0){
             int size = queue.Count;
             
             for(int i=0; i<size; i++){
-                var curCell = queue.Dequeue();
-                row = curCell[0];
-                col = curCell[1];
-                
+                var cur = queue.Dequeue();
+                row = cur[0];
+                col = cur[1];
                 if(grid[row][col] == 1){
-                    reachedHouses++;
-                    totalDistance+=dist;
+                    visitedHouses++;
+                    totalDist += distance;
+                    if(visitedHouses == houses)
+                        return totalDist;
                     continue;
                 }
                 
-                foreach(var dir in dirs){
-                    var nextRow = row + dir[0];
-                    var nextCol = col + dir[1];
-                    
-                    if(nextRow < rows && nextCol < cols && nextRow >= 0 && nextCol >= 0
-                       && !visited[nextRow, nextCol] && grid[nextRow][nextCol] != 2){
+                 foreach(var dir in dirs){
+                    int nextRow = row + dir[0], nextCol = col + dir[1];
+                    if(nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols
+                      && !visited[nextRow, nextCol] && grid[nextRow][nextCol] != 2){
+                         queue.Enqueue(new[]{nextRow, nextCol});
                         visited[nextRow, nextCol] = true;
-                        queue.Enqueue(new int[]{ nextRow, nextCol});
                     }
                 }
             }
-            dist++;
+            distance++;
         }
         
-        if(houses != reachedHouses){
-            for(row=0; row<rows; row++){
-                for(col=0; col<cols; col++){
-                    if(visited[row, col] && grid[row][col] == 0)
+        if(visitedHouses != houses){
+            for(row = 0; row < rows; row++){
+                for(col = 0; col < cols; col++){
+                    if(grid[row][col] == 0 && visited[row, col]){
                         grid[row][col] = 2;
+                    }
                 }
             }
             return int.MaxValue;
         }
-        
-        return totalDistance;
+        return totalDist;
     }
 }
