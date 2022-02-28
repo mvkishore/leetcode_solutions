c136d86cfca0e9fbaccedc9776233d9661a342ba
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-28 20:51:26 +0530

    Time: 766 ms (20.22%), Space: 51.6 MB (65.43%) - LeetHub

--- 827-making-a-large-island/827-making-a-large-island.cs
+++ 827-making-a-large-island/827-making-a-large-island.cs
@@ -3,13 +3,12 @@ public class Solution {
         int maxArea = 0;
         int rows = grid.Length, cols = grid[0].Length;
         Dictionary<int, int> areas = new Dictionary<int, int>();
-        bool[,] visited = new bool[rows, cols];
         int color = 2;
         
         for(int row=0; row<rows; row++){
             for(int col=0; col<cols; col++){
-                if(grid[row][col] == 1 && !visited[row, col]){
-                    int area = GetArea(grid, row, col, color, visited);
+                if(grid[row][col] == 1){
+                    int area = GetArea(grid, row, col, color);
                     areas.Add(color, area);
                     maxArea = Math.Max(maxArea, area);
                     color++;
@@ -43,20 +42,17 @@ public class Solution {
         return maxArea;
     }
     
-    private int GetArea(int[][] grid, int row, int col, int color, bool[, ] visited)
+    private int GetArea(int[][] grid, int row, int col, int color)
     {
         
-        visited[row, col] = true;
         grid[row][col] = color;
-        
         int area = 1;
         int[][] dirs = new int[][]{new []{0, 1}, new[]{0, -1}, new []{1, 0}, new[]{-1, 0}};
         foreach(var dir in dirs){
             var nextRow = row + dir[0];
             var nextCol = col + dir[1];
-            if(Valid(nextRow, nextCol, grid) && !visited[nextRow, nextCol] && grid[nextRow][nextCol] == 1){
-                area += GetArea(grid, nextRow, nextCol, color, visited);
-                
+            if(Valid(nextRow, nextCol, grid) && grid[nextRow][nextCol] == 1){
+                area += GetArea(grid, nextRow, nextCol, color);
             }
         }
         return area;
