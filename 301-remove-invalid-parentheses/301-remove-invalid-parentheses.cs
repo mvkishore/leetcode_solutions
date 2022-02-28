2a5770f526d79917c5167d4847aab8019464e7fa
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-28 19:31:46 +0530

    Time: 216 ms (80.22%), Space: 47.2 MB (15.83%) - LeetHub

--- 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cs
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cs
@@ -9,31 +9,31 @@ public class Solution {
         visited.Add(s);
         while(queue.Count > 0){
             var size = queue.Count;
-            
+
             for(int i=0; i<size; i++){
                 var cur = queue.Dequeue();
-                
-                if(IsValid(cur)){
+
+                if(IsValid(cur))
                     res.Add(cur);
-                }
                 
-                if(res.Count == 0){
-                   for(int j=0; j < cur.Length; j++) {
-                       if(cur[j] == '(' || cur[j] == ')')
-                       {
-                           if(j == 0 || (j > 0 && cur[j-1] != cur[j])) {
-                               var next = cur.Substring(0, j) + cur.Substring(j+1, cur.Length - j - 1);
-                               if(!visited.Contains(next)){
-                                   visited.Add(next);
-                                   queue.Enqueue(next);
-                               }
-                           }
+                if(res.Count > 0) continue;
+                
+                for(int j=0; j < cur.Length; j++) {
+                   if(cur[j] == '(' || cur[j] == ')')
+                   {
+                       if(j > 0 && cur[j-1] == cur[j]) continue;
+                       
+                       var next = cur.Substring(0, j) + cur.Substring(j+1, cur.Length - j - 1);
+                       if(!visited.Contains(next)){
+                           visited.Add(next);
+                           queue.Enqueue(next);
                        }
                    }
                 }
+                
+                if(res.Count > 0)
+                    return res;
             }
-            if(res.Count > 0)
-                return res;
         }
         return res;
     }
