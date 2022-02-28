ac0a61a2d3a3a37fe7b00c76391822c9313e93c5
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-02-28 20:49:49 +0530

    Time: 563 ms (67.55%), Space: 54 MB (23.94%) - LeetHub

--- 827-making-a-large-island/827-making-a-large-island.cs
+++ 827-making-a-large-island/827-making-a-large-island.cs
@@ -1,68 +1,70 @@
 public class Solution {
     public int LargestIsland(int[][] grid) {
-        int n = grid.Length, color = 2;
-        Dictionary<int, int> areas = new Dictionary<int, int>();
         int maxArea = 0;
+        int rows = grid.Length, cols = grid[0].Length;
+        Dictionary<int, int> areas = new Dictionary<int, int>();
+        bool[,] visited = new bool[rows, cols];
+        int color = 2;
         
-        bool[,] visited = new bool[n, n];
-        for(int row=0; row<n; row++){
-            for(int col=0; col<n; col++){
+        for(int row=0; row<rows; row++){
+            for(int col=0; col<cols; col++){
                 if(grid[row][col] == 1 && !visited[row, col]){
-                    var area = GetAreaAndPaint(grid, row, col, color, visited);
-                    areas.Add(color++, area);
+                    int area = GetArea(grid, row, col, color, visited);
+                    areas.Add(color, area);
                     maxArea = Math.Max(maxArea, area);
+                    color++;
                 }
             }
         }
+        int[][] dirs = new int[][]{new []{0, 1}, new[]{0, -1}, new []{1, 0}, new[]{-1, 0}};
         
-        for(int row=0; row<n; row++){
-            for(int col=0; col<n; col++){
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
                 if(grid[row][col] == 0){
                     int area = 1;
-                    HashSet<int> seenColor = new HashSet<int>();
-                    foreach(var next in GetNext(row, col, n))
-                    {
-                        color = grid[next.row][next.col];
-                        if(color > 1 && !seenColor.Contains(color)){
-                            area += areas[color];
-                            seenColor.Add(color);
+                    HashSet<int> seenColors = new HashSet<int>();
+                    
+                    foreach(var dir in dirs){
+                        var nextRow = row + dir[0];
+                        var nextCol = col + dir[1];
+                        
+                        if(Valid(nextRow, nextCol, grid)){
+                            color = grid[nextRow][nextCol];
+                            if(areas.ContainsKey(color) && !seenColors.Contains(color)){
+                                area += areas[color];
+                                seenColors.Add(color);
+                            }
                         }
                     }
                     maxArea = Math.Max(maxArea, area);
                 }
             }
         }
-        
         return maxArea;
     }
     
-    private int GetAreaAndPaint(int[][] grid, int row, int col, int color, bool[,] visited)
+    private int GetArea(int[][] grid, int row, int col, int color, bool[, ] visited)
     {
-        int area = 1;
-        visited[row, col] = true;
-        int n = grid.Length;
-        grid[row][col] = color;
-        foreach(var next in GetNext(row, col, n))
-        {
-            if(grid[next.row][next.col] == 1 && !visited[next.row, next.col])
-                area += GetAreaAndPaint(grid, next.row, next.col, color, visited);
-        }
         
+        visited[row, col] = true;
+        grid[row][col] = color;
+        
+        int area = 1;
+        int[][] dirs = new int[][]{new []{0, 1}, new[]{0, -1}, new []{1, 0}, new[]{-1, 0}};
+        foreach(var dir in dirs){
+            var nextRow = row + dir[0];
+            var nextCol = col + dir[1];
+            if(Valid(nextRow, nextCol, grid) && !visited[nextRow, nextCol] && grid[nextRow][nextCol] == 1){
+                area += GetArea(grid, nextRow, nextCol, color, visited);
+                
+            }
+        }
         return area;
     }
     
-    private List<(int row, int col)> GetNext(int row, int col, int n)
+    private bool Valid(int row, int col, int[][] grid)
     {
-        var nextPos = new List<(int x, int y)>();
-        var dirs = new int[][]{new int[]{1, 0}, new int[]{-1, 0},new int[]{0, 1},new int[]{0, -1}};
-        
-        foreach(var dir in dirs){
-            int nextRow = row + dir[0];
-            int nextCol = col + dir[1];
-            
-            if(nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < n)
-                nextPos.Add((nextRow, nextCol));
-        }
-        return nextPos;
+        int rows = grid.Length, cols = grid[0].Length;
+        return row < rows && row >= 0 && col >=0 && col < cols;
     }
 }
