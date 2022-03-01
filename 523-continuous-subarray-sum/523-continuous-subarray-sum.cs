599582befe4550385fa2d357881cd493d60cf15e
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-01 07:41:51 +0530

    Time: 330 ms (70.57%), Space: 50.9 MB (69.75%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.cs
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.cs
@@ -1,19 +1,20 @@
 public class Solution {
     public bool CheckSubarraySum(int[] nums, int k) {
-        int n = nums.Length, sum = 0;
-        Dictionary<int, int> mods = new Dictionary<int, int>();
-        mods.Add(0, -1);
+        int n = nums.Length;
+        int sum = 0;
+        Dictionary<int, int> seenSums = new Dictionary<int, int>();
+        seenSums.Add(0, -1);
         
-        for(int i = 0; i<n; i++){
+        for(int i=0; i< n; i++){
             sum += nums[i];
-            sum %= k;
-            if(mods.ContainsKey(sum))
-            {
-                if(i - mods[sum] > 1) return true;
-            }
-            else 
-                mods[sum] = i;
+            sum = sum %k;
+            if(seenSums.ContainsKey(sum)){
+                var prev = seenSums[sum];
+                if(i - prev > 1)
+                    return true;
+            }else seenSums.Add(sum, i);
         }
         return false;
     }
 }
+    
