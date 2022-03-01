608f5ab42b7d48dcb8bff6261cc387a740390dbe
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-01 09:13:25 +0530

    Time: 407 ms (34.08%), Space: 62.5 MB (48.88%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cs
+++ 41-first-missing-positive/41-first-missing-positive.cs
@@ -1,14 +1,37 @@
 public class Solution {
     public int FirstMissingPositive(int[] nums) {
-        int missing = 1;
-        HashSet<int> seenNums = new HashSet<int>();
+        int n = nums.Length;
+        bool oneExist = false;
         foreach(var num in nums){
-            seenNums.Add(num);
-            if(num == missing){
-                while(seenNums.Contains(missing))
-                    missing++;
+            if(num == 1){
+                oneExist = true;
+                break;
             }
         }
-        return missing;
+        
+        if(!oneExist) return 1;
+        
+        for(int i=0; i<n; i++){
+            if(nums[i] <= 0 || nums[i] > n)
+                nums[i] = 1;
+        }
+        
+        for(int i=0; i<n; i++){
+            int val = Math.Abs(nums[i]);
+            
+            if(val == n)
+                nums[0] = - Math.Abs(nums[0]);
+            else nums[val] = - Math.Abs(nums[val]);
+        }
+        
+        
+        for(int i=1; i<n; i++)
+            if(nums[i] > 0)
+                return i;
+        
+         if(nums[0] > 0)
+            return n;
+        
+        return n + 1;    
     }
-}
+}                               
