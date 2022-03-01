6450a958b511949977119e22b614a5347a0054e9
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-01 09:16:22 +0530

    Time: 72 ms (95.08%), Space: 40 MB (20.01%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cs
+++ 242-valid-anagram/242-valid-anagram.cs
@@ -1,15 +1,15 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
         int[] counter = new int[26];
+        
         foreach(var c in s)
-            counter[c-'a']++;
+            counter[c - 'a']++;
         foreach(var c in t)
             counter[c - 'a']--;
         
-        foreach(var n in counter)
-            if(n != 0)
+        foreach(var v in counter)
+            if(v != 0)
                 return false;
-        
         return true;
     }
 }
