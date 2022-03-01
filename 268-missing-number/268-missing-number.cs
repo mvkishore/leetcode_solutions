9870d43a15abba23f85966c367eb9078c3c84e9a
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-01 08:52:01 +0530

    Time: 101 ms (84.10%), Space: 39.9 MB (64.49%) - LeetHub

--- 268-missing-number/268-missing-number.cs
+++ 268-missing-number/268-missing-number.cs
@@ -1,11 +1,7 @@
 public class Solution {
     public int MissingNumber(int[] nums) {
         int n = nums.Length;
-        int missing = n;
-        for(int i=0; i<n; i++){
-            var val = nums[i] ^ i;
-            missing ^= val;
-        }
-        return missing;
+        var sum = nums.Sum();
+        return n * (n + 1) / 2 - sum;
     }
 }
