0841e771286e276430472905f7f5df41c512f10b
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-01 06:59:36 +0530

    Time: 193 ms (58.18%), Space: 43.9 MB (38.86%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.cs
+++ 249-group-shifted-strings/249-group-shifted-strings.cs
@@ -1,48 +1,38 @@
 public class Solution {
     public IList<IList<string>> GroupStrings(string[] strings) {
         Dictionary<string, List<string>> groups = new Dictionary<string, List<string>>();
-        
         foreach(var str in strings){
-            var hash = GetHash(str);
-            if(!groups.ContainsKey(hash))
-                groups.Add(hash, new List<string>());
-            
-            groups[hash].Add(str);
+            var key = GetKey(str);
+            if(!groups.ContainsKey(key))
+                groups.Add(key, new List<string>());
+            groups[key].Add(str);
         }
-        IList<IList<string>> res = new List<IList<string>>();
         
-        foreach(var group in groups)
-            res.Add(group.Value);
+        IList<IList<string>> res = new List<IList<string>>();
+        foreach(var value in groups.Values)
+            res.Add(value);
+        
         return res;
     }
     
-    private string GetHash(string str)
-    {
-        StringBuilder sb = new StringBuilder();
+    private string GetKey(string str){
+        StringBuilder key = new StringBuilder();
         int n = str.Length;
         for(int i=1; i<n; i++){
-            int diff = (26 - (str[i-1] - str[i])) %26;
-            sb.Append(diff);
-            sb.Append('.');
+            int d = str[i] - str[i-1];
+            if(d < 0)
+                d += 26;
+            key.Append(d);
+            key.Append("-");
         }
         
-        if(sb.Length == 0)
-            sb.Append('.');
+        if(key.Length > 0)
+            key.Length--;
         
-        return sb.ToString();
+        return key.ToString();
     }
 }
 
-/*
-"xac","zce"
--23.2 -23.2
 
- 1.1    1.1
-"ax" , "xy" 
-
-"ax"-> 24  "xy" ->1
-
-"xa" -> -23
-"zc" -> -23
-"a"-0  "v"-0
-*/
+    
+    
