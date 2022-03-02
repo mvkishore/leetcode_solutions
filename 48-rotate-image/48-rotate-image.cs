7722ceb9ca5ad1f89142206d4d70a58b1949f1a0
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-02 16:04:18 +0530

    Time: 154 ms (75.89%), Space: 41.1 MB (53.99%) - LeetHub

--- 48-rotate-image/48-rotate-image.cs
+++ 48-rotate-image/48-rotate-image.cs
@@ -1,24 +1,23 @@
 public class Solution {
     public void Rotate(int[][] matrix) {
-        int n = matrix.Length;
+        int rows = matrix.Length, cols = matrix[0].Length;
         
-        for(int row = 0; row < (n + 1) / 2; row++){
-            for(int col = 0; col < n / 2; col++){
+        for(int row = 0; row < (rows + 1) / 2; row++) {
+            for(int col = 0; col < cols / 2; col++) {
                 //top
                 int temp = matrix[row][col];
                 
                 //top <- left
-                matrix[row][col] = matrix[n - col - 1][row];
+                matrix[row][col] = matrix[cols - col - 1][row];
                 
-                //left <- down
-                matrix[n - col - 1][row] = matrix[n - row - 1][n - col - 1];
+                //left<- down
+                matrix[cols - col - 1][row] = matrix[rows - row - 1][cols - col - 1];
                 
-                //down <- right
-                matrix[n - row - 1][n - col - 1] = matrix[col][n - row - 1];
-                
-                //right <- top
-                matrix[col][n - row -1] = temp;
-                
+                //down<- right
+                matrix[rows - row - 1][cols - col - 1] = matrix[col][rows - row - 1];
+                    
+                //right<- top
+                matrix[col][rows - row - 1] = temp;
             }
         }
     }
