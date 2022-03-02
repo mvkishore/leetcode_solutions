68762fc67d3cd8e84c1e8adc5ebceb1550aacc8c
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-02 08:57:47 +0530

    Time: 195 ms (61.78%), Space: 46.4 MB (22.78%) - LeetHub

--- 636-exclusive-time-of-functions/636-exclusive-time-of-functions.cs
+++ 636-exclusive-time-of-functions/636-exclusive-time-of-functions.cs
@@ -1,27 +1,28 @@
 public class Solution {
     public int[] ExclusiveTime(int n, IList<string> logs) {
-        int[] times = new int[n];
-        Stack<int> stack = new Stack<int>();
-        int runningFunc = 0, time = 0;
+        Stack<int> callStack = new Stack<int>();
+        int[] res = new int[n];
+        int runningFunc = 0;
+        int seenTime = 0;
         foreach(var log in logs){
-            var values = log.Split(":");
-            var func = Convert.ToInt32(values[0]);
-            var isStart = values[1] == "start";
-            var curTime = Convert.ToInt32(values[2]);
+            var tokens = log.Split(":");
+            var func = Convert.ToInt32(tokens[0]);
+            var isStart = tokens[1] == "start";
+            var time = Convert.ToInt32(tokens[2]);
             
-            if(!isStart) 
-                curTime++;
-            times[runningFunc] += curTime - time;
-
-            time = curTime;
-            if(isStart) {
-                stack.Push(runningFunc);
+            if(!isStart) time++;
+            
+            res[runningFunc] += time - seenTime;
+            seenTime = time;
+            
+            if(isStart){
+                callStack.Push(runningFunc);
                 runningFunc = func;
             } else {
-                runningFunc = stack.Pop();
+                runningFunc = callStack.Pop();
             }
         }
         
-        return times;
+        return res;
     }
-}    
+}
