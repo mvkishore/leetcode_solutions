78744666bde7f55cc86f3f8286d2e1160c4eae73
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-06 09:47:17 +0530

    Time: 333 ms (13.76%), Space: 47.7 MB (43.39%) - LeetHub

--- 588-design-in-memory-file-system/588-design-in-memory-file-system.cs
+++ 588-design-in-memory-file-system/588-design-in-memory-file-system.cs
@@ -1,84 +1,70 @@
 public class FileSystem {
+    
     public class FileNode {
         public string Name;
-        public Dictionary<string, FileNode> childs;
+        public Dictionary<string, FileNode> Childs;
         public bool IsDirectory;
         public string Content;
         
-        public FileNode(string name, bool isDirectory)
-        {
+        public FileNode(string name, bool isDirectory){
             this.Name = name;
             this.IsDirectory = isDirectory;
-            childs = new Dictionary<string, FileNode>();
+            this.Childs = new Dictionary<string, FileNode>();
         }
     }
-    
+
     FileNode root;
-    
     public FileSystem() {
         root = new FileNode("/", true);
     }
     
     public IList<string> Ls(string path) {
-        var directories = path.Split("/");
+        var node = GetFileNode(path, false);
         
-        var trav = root;
-        for(int i=0; i < directories.Length; i++){
-            var cur = directories[i];
-            if(trav.childs.ContainsKey(cur))
-                trav = trav.childs[cur];
+        if(node == null)
+            return new List<string>();
+        
+        if(!node.IsDirectory){
+            return new List<string>{ node.Name };
         }
         
-        if(!trav.IsDirectory)
-            return new List<string>(){trav.Name};
-        
-        var res = new List<string>();
-        foreach(var name in trav.childs.Keys)
-            res.Add(name);
-        res.Sort();
-        return res;
+        var list = node.Childs;
+        return list.Keys.OrderBy(x => x).ToList();
     }
     
     public void Mkdir(string path) {
-        GetDir(path);
-    }
-    
-    private FileNode GetDir(string path){
-        var directories = path.Split("/");
-        var trav = root;
-        for(int i=0; i<directories.Length; i++){
-            var cur = directories[i];
-            if(!trav.childs.ContainsKey(cur))
-                trav.childs.Add(cur, new FileNode(cur, true));
-                
-            trav = trav.childs[cur];
-        }
-        return trav;
+        var node = GetFileNode(path, true);
+        
     }
     
     public void AddContentToFile(string filePath, string content) {
-        var indx = filePath.LastIndexOf("/");
-        var path = filePath.Substring(0, indx);
-        var node = GetDir(path);
-        var fileName = filePath.Substring(indx + 1);
-        
-        if(!node.childs.ContainsKey(fileName))
-            node.childs.Add(fileName, new FileNode(fileName, false));
-        
-        var fileNode = node.childs[fileName];
-        
-        fileNode.Content = fileNode.Content + content;
+        var node = GetFileNode(filePath, true);
+        node.IsDirectory = false;
+        node.Content += content;
     }
     
     public string ReadContentFromFile(string filePath) {
-        var indx = filePath.LastIndexOf("/");
-        var path = filePath.Substring(0, indx);
-        var fileName = filePath.Substring(indx + 1);
+        var node = GetFileNode(filePath, false);
+        if(node != null && !node.IsDirectory)
+            return node.Content;
+        return string.Empty;
+    }
+    
+    public FileNode GetFileNode(string path, bool create){
+        var tokens = path.Split("/", StringSplitOptions.RemoveEmptyEntries);
+        var trav = root;
         
-        var node = GetDir(path);
-        var fileNode = node.childs[fileName];
+        for(int i=0; i<tokens.Length; i++){
+            var cur = tokens[i];
+            if(!trav.Childs.ContainsKey(cur)){
+                if(!create)
+                    return null;
+                trav.Childs.Add(cur, new FileNode(cur, true));
+            }
+            trav = trav.Childs[cur];
+        }
         
-        return fileNode.Content;
+        return trav;
     }
 }
 
