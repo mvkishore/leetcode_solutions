02bec69cf786322d62c70b6233f69ab9c7c0d4ad
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-07 10:54:28 +0530

    Time: 204 ms (71.44%), Space: 46.5 MB (62.81%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cs
+++ 56-merge-intervals/56-merge-intervals.cs
@@ -1,29 +1,29 @@
 public class Solution {
     public int[][] Merge(int[][] intervals) {
-        Array.Sort(intervals, (a, b) => a[0] - b[0]);
+        Array.Sort(intervals, (a,b) => a[0] - b[0]);
+        
         var prev = intervals[0];
-        List<int[]> result = new List<int[]>();
-            
-        for(int i=1; i < intervals.Length; i++){
+        var list = new List<int[]>();
+        
+        for(int i=1; i<intervals.Length; i++){
             var cur = intervals[i];
-            if(IsOverlap(prev, cur))
-                prev = Merge(prev, cur);
-            else{
-                result.Add(prev);
+            if(IsOverlap(prev, cur)){
+                prev = Merge(cur, prev);
+            }else{
+                list.Add(prev);
                 prev = cur;
             }
         }
-        result.Add(prev);
-        return result.ToArray();
+        
+        list.Add(prev);
+        return list.ToArray();
     }
     
-    private bool IsOverlap(int[] a, int[] b)
-    {
-        return a[0] <= b[1] && b[0] <= a[1];
+    private bool IsOverlap(int[] a, int[] b){
+        return b[0] <= a[1];
     }
     
-    private int[] Merge(int[] a, int[] b)
-    {
-        return new int[]{ Math.Min(a[0], b[0]), Math.Max(a[1], b[1])};
+    private int[] Merge(int[] a, int[] b){
+        return new []{ Math.Min(a[0], b[0]), Math.Max(a[1], b[1])};
     }
 }
