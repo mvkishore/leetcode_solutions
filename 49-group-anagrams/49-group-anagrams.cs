39a4d8cd10b2b76a28176b5dbec1467602b6f6ec
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-07 20:36:49 +0530

    Time: 294 ms (39.02%), Space: 50.6 MB (34.19%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cs
+++ 49-group-anagrams/49-group-anagrams.cs
@@ -1,32 +1,27 @@
 public class Solution {
     public IList<IList<string>> GroupAnagrams(string[] strs) {
-        IList<IList<string>> res = new List<IList<string>>();
         Dictionary<string, IList<string>> groups = new Dictionary<string, IList<string>>();
         
         foreach(var str in strs){
-            var key = GetKey(str);
+            var key = Sort(str);
             if(!groups.ContainsKey(key))
                 groups.Add(key, new List<string>());
+            
             groups[key].Add(str);
         }
         
-        foreach(var group in  groups.Values)
-            res.Add(group);
-        return res;
+        return groups.Values.ToList();
     }
-    private string GetKey(string str) {
-        
-        int[] count = new int[26];
-        foreach(var c in str)
-            count[c - 'a']++;
+    
+    private string Sort(string s){
         StringBuilder sb = new StringBuilder();
+        int[] counter = new int[26];
+        foreach(var c in s)
+            counter[c - 'a']++;
         
-        for(int i=0; i< 26; i++){
-            while(count[i] > 0) {
-                sb.Append((char)(i + 'a'));
-                count[i]--;
-            }
-        }
+        for(int i=0; i<26; i++)
+            while(counter[i]-- > 0)
+                sb.Append((char) i + 'a');
         
         return sb.ToString();
     }
