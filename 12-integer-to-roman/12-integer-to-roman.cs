92d84555f7ba3e58d270a03306351e7d1113a10e
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-08 08:56:31 +0530

    Time: 123 ms (42.84%), Space: 37.8 MB (52.48%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cs
+++ 12-integer-to-roman/12-integer-to-roman.cs
@@ -1,63 +1,18 @@
 public class Solution {
-    Dictionary<int, char> AllSymbols = new Dictionary<int,char>(){
-        {1, 'I'},
-        {5, 'V'},
-        {10, 'X'},
-        {50, 'L'},
-        {100, 'C'},
-        {500, 'D'},
-        {1000, 'M'}
-    };
-    
-    
     public string IntToRoman(int num) {
-        int len = num.ToString().Length;
-        StringBuilder sb = new StringBuilder();
-        int mul = 1;
+        int[] values = new [] {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string[] symbols = new[]{"M", "CM", "D", "CD", "C", "XC", "L","XL","X", "IX","V", "IV", "I"};
         
-        while(len > 0){
-            int d = num % 10;
-            
-            if(d > 0)
-                sb.Insert(0, GetString(d * mul, mul));
-            
-            mul *= 10;
-            num = num / 10;
-            len--;
+        StringBuilder res = new StringBuilder();
+        int i = 0;
+        while(num > 0){
+            while(i < values.Length && num >= values[i]){
+                num -= values[i];
+                res.Append(symbols[i]);
+            }
+            i++;
         }
         
-        return sb.ToString();
-    }
-    
-    private string GetString(int n, int mul)
-    {
-        StringBuilder sb = new StringBuilder();
-        if(n == mul){
-            sb.Append(AllSymbols[mul]);
-        }else if(n < 4 * mul){
-           while(n > 0){
-                sb.Append(AllSymbols[1 * mul]);
-                n -= mul;
-            }
-        }else if(n < 5 * mul){
-            sb.Append(AllSymbols[1 * mul]);
-            sb.Append(AllSymbols[5 * mul]);
-        }
-        else if(n == 5 * mul){
-            sb.Append(AllSymbols[5 * mul]);
-            
-        }else if(n < 9 * mul){
-            sb.Append(AllSymbols[5 * mul]);
-            n -= 5*mul;
-            
-            while(n > 0){
-                sb.Append(AllSymbols[1 * mul]);
-                n -= mul;
-            }
-        } else {
-            sb.Append(AllSymbols[1 * mul]);
-            sb.Append(AllSymbols[10 * mul]);
-        }
-        return sb.ToString();
+        return res.ToString();
     }
 }
