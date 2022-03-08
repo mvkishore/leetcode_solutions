c9799c1ec556acb81038cd6b6510a34147975d4c
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-08 18:53:57 +0530

    Time: 198 ms (14.78%), Space: 39.2 MB (70.51%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cs
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cs
@@ -1,33 +1,36 @@
 public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.Length;
-        int n = nums2.Length;
+        int m = nums1.Length, n = nums2.Length;
         if(n < m)
             return FindMedianSortedArrays(nums2, nums1);
         
         int lo = 0, hi = m;
-        
         while(lo <= hi){
-            int nums1Mid = (lo + hi) / 2;
-            int nums2Mid = (m + n + 1) / 2 - nums1Mid;
+            int mid1 = (lo + hi) / 2;
+            int mid2 = (m + n + 1) / 2 - mid1;
             
-            int nums1LeftMax = nums1Mid == 0 ? int.MinValue : nums1[nums1Mid - 1];
-            int nums1RightMin = nums1Mid == m ? int.MaxValue : nums1[nums1Mid];
+            int lp1 = mid1 == 0 ? int.MinValue : nums1[mid1 - 1];
+            int lp2 = mid2 == 0 ? int.MinValue : nums2[mid2 - 1];
             
-            int nums2LeftMax = nums2Mid == 0 ? int.MinValue : nums2[nums2Mid - 1];
-            int nums2RightMin = nums2Mid == n ? int.MaxValue : nums2[nums2Mid];
+            int rp1 = mid1 == m ? int.MaxValue : nums1[mid1];
+            int rp2 = mid2 == n ? int.MaxValue : nums2[mid2];
             
-            if(nums1LeftMax <= nums2RightMin && nums2LeftMax <= nums1RightMin){
-                if((m+n) % 2 == 0)
-                    return (Math.Max(nums1LeftMax, nums2LeftMax) + Math.Min(nums1RightMin, nums2RightMin)) / 2.0;
-                return Math.Max(nums1LeftMax, nums2LeftMax);
-            }
-            else if(nums1LeftMax > nums2RightMin)
-                hi = nums1Mid - 1;
+            if(lp1 <= rp2 && lp2 <= rp1){
+                if((m + n) % 2 == 0)
+                    return (Math.Max(lp1, lp2) + Math.Min(rp1, rp2)) / 2.0;
+                return Math.Max(lp1, lp2);
+            }else if(lp1 > rp2)
+                hi = mid1 - 1;
             else
-                lo = nums1Mid + 1;
+                lo = mid1 + 1;
         }
-        
         return -1;
     }
 }
+/*
+lp1      rp1
+[X X X X | X X X X X]
+lp2     rp2
+[X X X | X X X ]
+  */  
+  
