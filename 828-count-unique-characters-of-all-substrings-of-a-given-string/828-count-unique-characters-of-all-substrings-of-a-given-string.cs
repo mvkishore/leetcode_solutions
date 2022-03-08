1af8e754326feb462ebe8da2cbd6b2ded98362e0
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-08 19:51:10 +0530

    Time: 76 ms (87.30%), Space: 43.9 MB (5.82%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cs
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cs
@@ -2,9 +2,7 @@
 // - Brute Force: O(n^2 * n) ==> O(n^3)
 // - sum(f(i))
 // f(i) ==> number of string that s[i] is unique
-
 // How to compute f(i)?
-
 //   ABC
   
 //   XXX B XXXXX B XXX B XX
@@ -15,37 +13,38 @@
     ==>For First B,  f(B) = (6 - 3) * (3 - (-1))
     ==>For Second B,  f(B) = (9 - 6) * (6 - 3)
     ==>For Third B,  f(B) = (N - 9) * (9 - 6)
-
 */
 
 public class Solution {
     public int UniqueLetterString(string s) {
-        int[][] last2Indices = new int[26][];
-        int res = 0;
+        int N = s.Length;
         
-        for(int i=0; i<26; i++)
-            last2Indices[i] = new int[]{-1, -1};
+        int[][] last2Occurences = new int[26][];
         
-        for(int i=0; i<s.Length; i++){
-            var c = s[i] - 'A';
-            var lastOccurances = last2Indices[c];
-            
-            int m = lastOccurances[1] - lastOccurances[0];
-            int n = i - lastOccurances[1];
-            
-            res += m *n;
-            last2Indices[c] = new []{lastOccurances[1], i};
+        for(int i=0; i < 26; i++){
+            last2Occurences[i] = new []{-1, -1};
         }
         
-        int N = s.Length;
-        for(int i=0; i<26; i++){
-            var lastOccurances = last2Indices[i];
-            int m = (lastOccurances[1] - lastOccurances[0]);
-            int n = (N - lastOccurances[1]);
-            res += m *n;
+        int res = 0;
+        for(int i=0; i < N; i++){
+            var c = s[i] - 'A';
+            var cur = last2Occurences[c];
+            
+            int m = cur[1] - cur[0];
+            int n = i - cur[1];
+            
+            res += m * n;
+            last2Occurences[c] = new[]{cur[1], i};
+        }
+        
+        for(int i=0; i <26; i++){
+            var cur = last2Occurences[i];
+            
+            int m = cur[1] - cur[0];
+            int n = N - cur[1];
+            
+            res += m * n;
         }
         return res;
     }
 }
-
-   
