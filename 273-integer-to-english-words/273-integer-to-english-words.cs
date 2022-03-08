c49330db2998844f5b69459aca0662820b53535b
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-08 19:28:00 +0530

    Time: 93 ms (61.14%), Space: 37.9 MB (17.71%) - LeetHub

--- 273-integer-to-english-words/273-integer-to-english-words.cs
+++ 273-integer-to-english-words/273-integer-to-english-words.cs
@@ -1,137 +1,127 @@
 public class Solution {
+    Dictionary<int, string> LessThan20 = new Dictionary<int, string>(){
+        {10, "Ten"},
+        {11, "Eleven"},
+        {12, "Twelve"},
+        {13, "Thirteen"},
+        {14, "Fourteen"},
+        {15, "Fifteen"},
+        {16, "Sixteen"},
+        {17, "Seventeen"},
+        {18, "Eighteen"},
+        {19, "Nineteen"}
+    };
+    Dictionary<int, string> Tens = new Dictionary<int, string>(){
+        {1, "Ten"},
+        {2, "Twenty"},
+        {3,"Thirty"},
+        {4,"Forty"},
+        {5,"Fifty"},
+        {6,"Sixty"},
+        {7,"Seventy"},
+        {8,"Eighty"},
+        {9,"Ninety"}
+    };
+    Dictionary<int, string> Ones = new Dictionary<int, string>(){
+        {1, "One"},
+        {2, "Two"},
+        {3, "Three"},
+        {4, "Four"},
+        {5, "Five"},
+        {6, "Six"},
+        {7, "Seven"},
+        {8, "Eight"},
+        {9, "Nine"}
+    };
+    
     public string NumberToWords(int num) {
         if(num == 0)
-            return Get1DigitNumStr(num);
-        
-        int THOUSAND = 1000, MILLION = 1000 * THOUSAND, BILLION = 1000 * MILLION;
-        
-        int billions = num / BILLION;
-        int millions = (num - billions * BILLION) / MILLION;
-        int thousands = (num - billions * BILLION - millions * MILLION) / THOUSAND;
-        int remaining = num - billions * BILLION - millions * MILLION - thousands * THOUSAND;
+            return "Zero";
         
         StringBuilder sb = new StringBuilder();
+        
+        int billions = num / 1000000000;
+        num = num % 1000000000;
         if(billions > 0){
-            sb.Append(GetNumberStr(billions));
+            sb.Append(ThreeNumToWord(billions));
             sb.Append(" Billion");
         }
         
+        int millions = num / 1000000; // 9
+        num = num % 1000000;// 100
         if(millions > 0){
             if(sb.Length > 0)
                 sb.Append(" ");
-            
-            sb.Append(GetNumberStr(millions));
+            sb.Append(ThreeNumToWord(millions));
             sb.Append(" Million");
         }
         
+        int thousands = num / 1000;
+        num = num % 1000;
         if(thousands > 0){
             if(sb.Length > 0)
                 sb.Append(" ");
-            
-            sb.Append(GetNumberStr(thousands));
+            sb.Append(ThreeNumToWord(thousands));
             sb.Append(" Thousand");
         }
+        if(num > 0){
+            if(sb.Length > 0)
+                sb.Append(" ");
+            sb.Append(ThreeNumToWord(num));
+        }
+        return sb.ToString();
+    }
+    
+    private string ThreeNumToWord(int num){
+        int hundreds = num / 100; // 9
+        num = num % 100;// 100
         
-        if(remaining > 0){
+        StringBuilder sb = new StringBuilder();
+        if(hundreds > 0){
+            sb.Append(Ones[hundreds]);
+            sb.Append(" Hundred");
+        }
+        
+        if(num > 0){
+            if(sb.Length > 0)
+                sb.Append(" ");
+            sb.Append(TwoNumToWord(num));
+        }
+        return sb.ToString();
+    }
+    
+    private string TwoNumToWord(int num){
+        StringBuilder sb = new StringBuilder();
+        if(num >= 10 && num < 20){
+            sb.Append(LessThan20[num]);
+            return sb.ToString();
+        }
+        
+        int tens = num / 10;
+        num = num % 10;
+        
+        if(tens > 0){
+            if(sb.Length > 0)
+                sb.Append(" ");
+            sb.Append(Tens[tens]);
+        }
+        
+        if(num > 0){
             if(sb.Length > 0)
                 sb.Append(" ");
             
-            sb.Append(GetNumberStr(remaining));
-        }
-        
-        return sb.ToString();
-    }
-    //1-999
-    //1-99
-    //100s
-    //
-    //923
-    //23
-    public string GetNumberStr(int n)
-    {
-        int hundred = n / 100;
-        int rem = n % 100;
-        
-        StringBuilder sb = new StringBuilder();
-        if(hundred > 0){
-            sb.Append(Get2DigitNumStr(hundred));
-            sb.Append(" Hundred");
-            
-            if(rem > 0){
-                sb.Append(" ");
-                sb.Append(Get2DigitNumStr(rem));
-            }
-        } else {
-            sb.Append(Get2DigitNumStr(rem));
+            sb.Append(Ones[num]);
         }
         return sb.ToString();
-    }
-    
-    public string Get2DigitNumStr(int n){
-        int tens = n / 10;
-        int rem = n % 10;
-        
-        StringBuilder sb = new StringBuilder();
-        if(tens > 0){
-            if(n > 10 && n < 20){
-                sb.Append(GetLessTwentyNumStr(n));
-            }
-            else if(rem != 0)
-            {
-                sb.Append(GetTensNumStr(tens));
-                sb.Append(" ");
-                sb.Append(Get1DigitNumStr(rem));
-            } else {
-                sb.Append(GetTensNumStr(tens));
-            }
-        } else {
-            sb.Append(Get1DigitNumStr(rem));
-        }
-        return sb.ToString();
-    }
-    
-    private string GetTensNumStr(int num){
-        switch(num) {
-            case 1: return "Ten";
-            case 2: return "Twenty";
-            case 3: return "Thirty";
-            case 4: return "Forty";
-            case 5: return "Fifty";
-            case 6: return "Sixty";
-            case 7: return "Seventy";
-            case 8: return "Eighty";
-            case 9: return "Ninety";
-        }
-        return "";
-    }
-    
-    private string GetLessTwentyNumStr(int num){
-        switch(num){
-            case 11: return "Eleven";
-            case 12: return "Twelve";
-            case 13: return "Thirteen";
-            case 14: return "Fourteen";
-            case 15: return "Fifteen";
-            case 16: return "Sixteen";
-            case 17: return "Seventeen";
-            case 18: return "Eighteen";
-            case 19: return "Nineteen";
-        }
-        return  "";
-    }
-    private string Get1DigitNumStr(int num){
-        switch(num) {
-          case 0: return "Zero";
-          case 1: return "One";
-          case 2: return "Two";
-          case 3: return "Three";
-          case 4: return "Four";
-          case 5: return "Five";
-          case 6: return "Six";
-          case 7: return "Seven";
-          case 8: return "Eight";
-          case 9: return "Nine";
-        }
-        return "";
     }
 }
+/*
+1--9 => 
+11-19=>
+10--90=>
+
+1234567 => 
+               
+    567 => 5 hundred 
+    67 => sixty seven
+    */
