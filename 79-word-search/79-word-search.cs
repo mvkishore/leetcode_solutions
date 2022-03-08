493a96dc603e5cab88b3c30e2dbb283be3157d80
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-08 08:19:07 +0530

    Time: 443 ms (38.31%), Space: 39.5 MB (70.65%) - LeetHub

--- 79-word-search/79-word-search.cs
+++ 79-word-search/79-word-search.cs
@@ -1,53 +1,40 @@
 public class Solution {
     public bool Exist(char[][] board, string word) {
         int rows = board.Length, cols = board[0].Length;
-        for(int row = 0; row< rows; row++){
+        
+        for(int row = 0; row < rows; row++){
             for(int col = 0; col < cols; col++){
                 if(board[row][col] == word[0]){
-                    bool[,] visited = new bool[rows, cols];
-                    if(Exist(board, row, col, 0, word, visited))
+                    var visited = new bool[rows, cols];
+                    if(Find(word, 0, board, row, col, visited))
                         return true;
                 }
             }
         }
-        
         return false;
     }
-    
-    private bool Exist(char[][] board, int row, int col, int index, string word, bool[,] visited){
-        if(index == word.Length)
+    private bool Find(string word, int indx, char[][] board, int row, int col, bool[,] visited)
+    {
+        int rows = board.Length, cols = board[0].Length;
+        if(indx == word.Length)
             return true;
         
-        if(index == word.Length - 1 && board[row][col] == word[index])
-            return true;
         
-        if(board[row][col] != word[index])
+        if(row < 0 || row >= rows || col < 0 || col >= cols || visited[row, col])
+            return false;
+        
+        if(board[row][col] != word[indx])
             return false;
         
         visited[row, col] = true;
-        foreach(var next in GetNextMove(row, col, board))
-        {
-            int nextRow = next[0], nextCol = next[1];
-            if(!visited[nextRow, nextCol] 
-               && Exist(board, nextRow, nextCol, index + 1, word, visited)){
-                return true;
-            }
-        }
+        
+        if( Find(word, indx + 1, board, row + 1, col, visited) ||
+            Find(word, indx + 1, board, row - 1, col, visited) ||
+            Find(word, indx + 1, board, row, col + 1, visited) ||
+            Find(word, indx + 1, board, row, col - 1, visited))
+            return true;
+            
         visited[row, col] = false;
         return false;
-    }
-    private List<int[]> GetNextMove(int row, int col, char[][] board)
-    {
-        int rows = board.Length, cols = board[0].Length;
-        List<int[]> nextMoves = new List<int[]>();
-        var dirs = new int[][] {new []{0, 1}, new []{1, 0},new []{-1, 0},new []{0, -1}};
-        
-        foreach(var dir in dirs){
-            int nextRow = row + dir[0], nextCol = col + dir[1];
-            if(nextRow < rows && nextRow >= 0 && nextCol < cols && nextCol >= 0){
-                nextMoves.Add(new []{nextRow, nextCol});
-            }
-        }
-        return nextMoves;
     }
 }
