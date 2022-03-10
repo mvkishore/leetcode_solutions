eb588fd9f04b3c22d6e22f43b0a628c3ccbf68d4
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-10 21:09:48 +0530

    Time: 128 ms (33.94%), Space: 36.9 MB (36.10%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cs
+++ 299-bulls-and-cows/299-bulls-and-cows.cs
@@ -1,28 +1,23 @@
 public class Solution {
     public string GetHint(string secret, string guess) {
         int n = secret.Length;
-        HashSet<int> bulls = new HashSet<int>();
         int[] counter = new int[10];
-        
-        for(int i=0; i<n; i++)
-        {
-            if(secret[i] == guess[i])
-                bulls.Add(i);
-        }
-        
+
         for(int i=0; i<n; i++) {
-            if(!bulls.Contains(i))
+            if(secret[i] != guess[i])
                 counter[secret[i] - '0']++;
         }
         
-        int cows = 0;
+        int cows = 0, bulls = 0;
         for(int i=0; i<n;i++){
-            if(!bulls.Contains(i) && counter[guess[i] - '0'] > 0){
+            if(secret[i] == guess[i]){
+                bulls++;
+            }else if(counter[guess[i] - '0'] > 0){
                 counter[guess[i] - '0']--;
                 cows++;
             }
         }
         
-        return $"{bulls.Count}A{cows}B";
+        return $"{bulls}A{cows}B";
     }
 }
