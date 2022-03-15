33d0a202263fb09087e02f83deb8e28ed91c4c0a
Venkata Kishore Marisetty <kishore9700@gmail.com>
2022-03-15 19:04:08 +0530

    Time: 172 ms (22.63%), Space: 41.3 MB (44.37%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cs
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cs
@@ -1,33 +1,30 @@
 public class Solution {
     public string MinRemoveToMakeValid(string s) {
-        char[] charArray = s.ToCharArray();
-        charArray = RemoveToMakeValid(charArray, '(', ')');
+        var chars = s.ToCharArray();
+        chars = Remove(chars, '(', ')');
+        Array.Reverse(chars);
+        chars = Remove(chars, ')', '(');
+        Array.Reverse(chars);
         
-        Array.Reverse(charArray);
-        charArray = RemoveToMakeValid(charArray, ')', '(');
-        Array.Reverse(charArray);
-        
-        return new string(charArray);
+        return new string(chars);
     }
     
-    private char[] RemoveToMakeValid(char[] charArray, char open, char close)
+    public char[] Remove(char[] charArray, char open, char close)
     {
-        int n = charArray.Length;
-        List<char> res = new List<char>();
         int count = 0;
-        
-        for(int i=0; i<n; i++){
-            if(charArray[i] == open){
+        var res = new List<char>();
+        foreach(var c in charArray){
+            if(c == open){
                 count++;
-            }else if(charArray[i] == close){
+            } else if(c == close){
+                if(count <= 0)
+                    continue;
                 count--;
             }
             
-            if(count >= 0)
-                res.Add(charArray[i]);
-            else
-                count = 0;
+            res.Add(c);
         }
+        
         return res.ToArray();
     }
 }
